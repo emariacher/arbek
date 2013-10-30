4faeea6774118079055371da8bd0beb3a6d9585f
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2013-10-30 07:26:21 +0100

    EulerFactorielles cleaning

--- euler/src/euler/EulerFactorielle.scala
+++ euler/src/euler/EulerFactorielle.scala
@@ -1,5 +1,8 @@
 package euler
 
+import kebra.MyLog._
+import EulerFactorielle._
+
 object EulerFactorielle {
 
     def fact(n: BigInt): BigInt = {
@@ -32,4 +35,17 @@ class EulerFactorielleNext(val i: Int, val prev: BigInt) {
     def f: BigInt = i * prev
 }
 
+object Permutations extends App {
+    List("AAAAA", "AOOOO", "AAOOO", "AAOOL", "AAAOL", "AAOL", "AAOOLL", "AAAOLL", "AAAALL", "AAAAOL", "AAABBCCCDEE").foreach(s => myAssert2(permLength(s), s.permutations.toList.length))
+    myPrintDln("Alles klar!")
 
+    def permLength(s: String): BigInt = {
+        var melem = Map[Char, Int]()
+        var lastChar = s.head
+        s.foreach((c: Char) => melem = melem + (c -> (melem.getOrElse(c, 0) + 1)))
+        val result = fact(s.length) / melem.toList.map((t: (Char, Int)) => fact(t._2)).product
+
+        //myPrintIt((s, result, melem, s.permutations.toList.length))
+        result
+    }
+}
