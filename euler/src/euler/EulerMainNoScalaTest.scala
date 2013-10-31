c95a9bcabff1823c21fb04f5f59f3d9a65510964
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2013-10-31 14:36:24 +0100

    Euler241

--- euler/src/euler/EulerMainNoScalaTest.scala
+++ euler/src/euler/EulerMainNoScalaTest.scala
@@ -27,7 +27,9 @@ object EulerMainNoScalaTest extends App {
 }
 
 class Euler241 {
-    myErrPrintDln((1 until 100000).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
+    myErrPrintDln((2 until 1000000 by 2).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
+    myErrPrintDln((4320 until 10000000 by (24*9)).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
+    myErrPrintDln((4680 until 100000000 by (72*13)).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
 
     class DoZeJob(bi: BigInt) {
         val div = new EulerDiv(bi)
@@ -35,7 +37,7 @@ class Euler241 {
         val pfbi = perfquot(bi, sbi)
         val spfbi = pfbi.toString
         val kp5 = spfbi.substring(spfbi.length - 2) == ".5"
-        myPrintln(bi, sbi, pfbi, kp5)
+        //myPrintln(bi, sbi, pfbi, kp5)
 
         def toString2 = "[" + bi + "," + div.primes + "," + (new EulerDivisors(div).getFullDivisors).sorted + "," + pfbi + "]"
         override def toString = "[" + bi + "," + div.primes + "," + pfbi + "]"
