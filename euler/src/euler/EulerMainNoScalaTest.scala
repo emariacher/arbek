b4f727abfc71d774dff4629ea635d7708a2bce58
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2013-11-01 04:38:04 +0100

    Euler241 aka stuff failing
    Mais je ne m'avoue pas vaincu!

--- euler/src/euler/EulerMainNoScalaTest.scala
+++ euler/src/euler/EulerMainNoScalaTest.scala
@@ -1,6 +1,7 @@
 package euler
 
 import kebra.MyLog._
+import kebra.MyLog
 import kebra.MyFileChooser
 import scala.collection.immutable.TreeSet
 import java.util.Calendar
@@ -16,25 +17,34 @@ import java.io.BufferedInputStream
 import scala.collection.JavaConversions._
 import scala.collection.immutable.ListSet
 import Permutations._
+import akka.actor._
+import akka.routing.RoundRobinRouter
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 
 object EulerMainNoScalaTest extends App {
-    var t_start = Calendar.getInstance
-    myPrintDln("Hello World!")
+    new MyLog("EulerMainNoScalaTest", new File("log"), "log")
+    try {
+        var t_start = Calendar.getInstance
+        myPrintDln("Hello World!")
 
-    new Euler241
+        new Euler241
 
-    timeStamp(t_start, "Au revoir Monde!")
+        timeStamp(t_start, "Au revoir Monde!")
+    } catch {
+        case ex: Exception => {
+            println("\n" + ex)
+            println("\n" + ex.getStackTrace.mkString("\n  ","\n  ","\n  ") + "\n")
+        }
+    } finally {
+        println("\nHere!")
+        MyLog.getMylog.closeFiles
+        println("\nThere!")
+    }
 }
 
 class Euler241 {
-    /*myErrPrintDln((2     until 100000 by 2).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
-    myErrPrintDln((4320  until 10000000 by (8*9)).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
-    myErrPrintDln((4680  until 100000000 by (8*9*13)).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
-    myErrPrintDln((BigInt(26208) until BigInt(1000000)* BigInt(1000) by BigInt(32*9*13)).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
-    myErrPrintDln((BigInt(8910720) until BigInt(1000000)* BigInt(10000) by BigInt(32*9*13*35)).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))
-    myErrPrintDln((BigInt(0) until BigInt(1000000)* BigInt(10000) by BigInt(32*9*11*35)).map(new DoZeJob(_)).filter(_.kp5).mkString("\n"))*/
-
-    var result = ListSet[DoZeJob]() ++ loop(List(BigInt(2)), List(BigInt(100000)), List(2))
+    /*var result = ListSet[DoZeJob]() ++ loop(List(BigInt(2)), List(BigInt(100000)), List(2))
     myErrPrintln(result)
     result = result ++ loop(List(BigInt(4320)), List(BigInt(10000000)), List(8, 9))
     myErrPrintln(result)
@@ -49,7 +59,20 @@ class Euler241 {
     result = result ++ loop(List(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 5, 7, 13, 23), List(BigInt(1000000000), BigInt(10000)), List(1024, 9, 13, 35))
     myErrPrintln(result)
     result = result ++ loop(List(BigInt(0)), List(BigInt(1000000000), BigInt(10000)), List(1024, 9, 11, 35))
-    myErrPrintln(result.toList.sortBy(_.bi).mkString("\n"))
+    myErrPrintln(result.toList.sortBy(_.bi).mkString("\n"))*/
+
+    val lj = List(
+        (List(BigInt(2)), List(BigInt(100000)), List[BigInt](2)),
+        (List(BigInt(4320)), List(BigInt(10000000)), List[BigInt](8, 9)),
+        (List(BigInt(26208)), List(BigInt(1000000), BigInt(1000)), List[BigInt](32, 9, 13)),
+        (List(BigInt(8910720)), List(BigInt(1000000), BigInt(10000)), List[BigInt](32, 9, 13, 35)),
+        (List(BigInt(0)), List(BigInt(1000000), BigInt(1000)), List[BigInt](32, 9, 11)),
+        (List(BigInt(8910720)), List(BigInt(10000000), BigInt(10000)), List[BigInt](128, 9, 13, 35)),
+        (List[BigInt](2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 5, 7, 13, 23), List(BigInt(1000000000), BigInt(10000)), List[BigInt](1024, 9, 13, 35)),
+        (List(BigInt(0)), List(BigInt(1000000000), BigInt(10000)), List[BigInt](1024, 9, 11, 35)))
+
+    var nrOfWorkers = 4
+    GoLoops(nrOfWorkers, lj)
 
     class DoZeJob(val bi: BigInt) {
         val div = new EulerDiv(bi)
@@ -79,7 +102,7 @@ class Euler241 {
                 myPrintln("\n    " + dzj)
                 result = result + dzj
             }
-            if (count2 % 100 == 99) {
+            if (count2 % 10000 == 99) {
                 print(".")
             }
 
@@ -87,7 +110,7 @@ class Euler241 {
             count2 += 1
         }
         timeStamp(t_start1, (start.mkString("*"), end.mkString("*"), inc.mkString("*")).toString)
-        myErrPrintln(result.toList.sortBy(_.bi).mkString("\n"))
+        myErrPrintDln((start.mkString("*"), end.mkString("*"), inc.mkString("*")).toString, result.toList.sortBy(_.bi).mkString("\n", "\n", "\n"))
         result
     }
 
@@ -95,5 +118,80 @@ class Euler241 {
     def sigma(bi: BigInt, ed: EulerDiv) = (new EulerDivisors(ed).getFullDivisors).sum
     def perfquot(bi: BigInt) = sigma(bi).toDouble / bi.toDouble
     def perfquot(bi: BigInt, sbi: BigInt) = sbi.toDouble / bi.toDouble
+
+    sealed trait akkaDoZeJob
+    case object Demarre extends akkaDoZeJob
+    case class GoLoop(start: List[BigInt], end: List[BigInt], inc: List[BigInt]) extends akkaDoZeJob
+    case class Resultat(result: ListSet[DoZeJob]) extends akkaDoZeJob
+    case class ResultatsLoops(results: ListSet[DoZeJob])
+
+    class Worker extends Actor {
+
+        /*def echappeJeton(j: Jeton): Int = {
+            while (j.avance != StateMachine.termine) {
+                //myPrintDln("    ("+j+", "+j.cnt+")")
+            }
+            //myPrintDln("   -("+j+", "+j.cnt+")")
+            j.cnt
+        }*/
+
+        def receive = {
+            case GoLoop(start, end, inc) =>
+                sender ! Resultat(loop(start, end, inc))
+        }
+    }
+
+    class Master(nrOfWorkers: Int, lj: List[(List[BigInt], List[BigInt], List[BigInt])], listener: ActorRef) extends Actor {
+
+        var nrOfResults: Int = _
+        var results = ListSet.empty[DoZeJob]
+
+        val workerRouter = context.actorOf(
+            Props[Worker].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
+
+        def receive = {
+            case Demarre =>
+                lj.foreach((t: (List[BigInt], List[BigInt], List[BigInt])) =>
+                    workerRouter ! GoLoop(t._1, t._2, t._3))
+            case Resultat(result) =>
+                //myPrintDln(" --("+c+", "+cnt+")")
+                results = results ++ result
+                nrOfResults += 1
+                if (nrOfResults == lj.size) {
+                    //myPrintDln("Send the result to the listener")
+                    listener ! ResultatsLoops(results)
+                    //myPrintDln("Stops this actor and all its supervised children")
+                    context.stop(self)
+                }
+        }
+
+    }
+
+    class Listener extends Actor {
+        def receive = {
+            case ResultatsLoops(results) =>
+                myErrPrintDln(results.toList.sortBy(_.bi).mkString("\n", "\n", "\n"))
+                context.system.shutdown()
+        }
+    }
+
+    def GoLoops(nrOfWorkers: Int, lj: List[(List[BigInt], List[BigInt], List[BigInt])]) {
+        // Create an Akka system
+        val system = ActorSystem("goLoops")
+
+        //myErrPrintDln("create the result listener, which will print the result and shutdown the system")
+        //val listener = system.actorOf(Props[Listener], name = "listener")
+        val listener = system.actorOf(Props(new Listener))
+
+        //myErrPrintDln("create the master")
+        val master = system.actorOf(Props(new Master(
+            nrOfWorkers, lj, listener)),
+            name = "master")
+
+        //myErrPrintDln("start the calculation")
+        master ! Demarre
+
+    }
+
 }
 
---
+++ euler/src/euler/akkaStuff.scala
@@ -0,0 +1,26 @@
+package euler
+
+import akka.actor.{ ActorSystem, Actor, Props }
+
+case object Start
+
+object akkaStuff extends App {
+    val system = ActorSystem()
+    system.actorOf(Props[HelloActor]) ! Start
+}
+
+class HelloActor extends Actor {
+    val worldActor = context.actorOf(Props[WorldActor])
+    def receive = {
+        case Start => worldActor ! "Hello"
+        case s: String =>
+            println("Received message: %s".format(s))
+            context.system.shutdown()
+    }
+}
+
+class WorldActor extends Actor {
+    def receive = {
+        case s: String => sender ! s.toUpperCase + " world!"
+    }
+}
