b2c3599fa67467684ec4835f166793c604738561
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2013-11-03 14:02:50 +0100

    Euler114

--- euler/src/euler/EulerMainNoScalaTest.scala
+++ euler/src/euler/EulerMainNoScalaTest.scala
@@ -44,16 +44,16 @@ object EulerMainNoScalaTest extends App {
 }
 
 class Euler114 {
-    new DoZeJob1(9)
+    new DoZeJob1(11)
 
     class DoZeJob1(len: Int) {
-        val lcheck = Map(1 -> 1, 2 -> 1, 3 -> 2, 4 -> 4, 5 -> 7, 6 -> 11, 7 -> 17, 8 -> 27, 9 -> 0)
-        val s1 = (1 to len).map(i => "A").toList.mkString
+        val lcheck = Map(1 -> 1, 2 -> 1, 3 -> 2, 4 -> 4, 5 -> 7, 6 -> 11, 7 -> 17, 8 -> 27, 9 -> 43, 10 -> 67)
+        val s1 = (1 to len).map(i => "0").toList.mkString
         val root: ListSet[String] = getRoot2
         myPrintDln(len, root)
 
-        val byCountNotA = root.groupBy(z => z.count(_ != 'A'))
-        myPrintDln(byCountNotA)
+        val byCountNot0 = root.groupBy(z => z.count(_ != '0'))
+        myPrintDln(byCountNot0.mkString("\n"))
         val zfinal = getFinal1
         val result = getFinal1.sum
         myErrPrintDln(len, result)
@@ -62,36 +62,41 @@ class Euler114 {
         def getRoot2 = {
             var root = ListSet.empty[String]
             if (len == 1) {
-                root = ListSet("A")
+                root = ListSet("0")
             } else {
-                val fromPrevRoot = new DoZeJob1(len - 1).root.map(s => s + "A")
-                root = fromPrevRoot ++ fromPrevRoot.map(_.replaceFirst("AAA", "3"))
+                val fromPrevRoot = new DoZeJob1(len - 1).root.map(s => s + "0")
+                root = fromPrevRoot ++ fromPrevRoot.map(_.replaceFirst("000", "3"))
             }
             if (len > 2) {
-                root = root + len.toString
+                root = root + ubersetz(len)
             }
             root
         }
 
         def getFinal1 = {
-            byCountNotA.toList.map((z: (Int, ListSet[String])) => {
+            byCountNot0.toList.map((z: (Int, ListSet[String])) => {
                 z._1 match {
                     case 0 => 1
                     case 1 => {
                         val y = z._2.toList.map(permLength)
-                        myPrintDln(z._1, y)
+                        myPrintln("count0["+z._1+"] ", y)
                         y.sum.toInt
                     }
                     case 2 => {
                         val y = z._2.map(_.permutations.toList).flatten
-                        val x = y.filter(_.toList.sliding(2).toList.filter(_.count(_ != 'A') > 1).isEmpty).toList
-                        myPrintDln(z._1, y, x)
+                        val x = y.filter(_.toList.sliding(2).toList.filter(_.count(_ != '0') > 1).isEmpty).toList.sorted
+                        //myPrintIt("count0["+z._1+"] ", y.toList.sorted)
+                        myPrintIt("count0["+z._1+"] ", x.toList)
                         x.length
                     }
-                    case _ => 0
+                    case _ =>  0
                 }
             })
         }
+
+        def ubersetz(i: Int) = vubersetz.substring(i, i + 1)
+        def findValue(s: String) = vubersetz.indexOf(s.head)
+        final val vubersetz = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     }
 }
 
