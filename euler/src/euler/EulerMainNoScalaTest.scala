89cc929ef612475237ede7874cc0916b68b89d08
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2013-11-03 20:41:14 +0100

    Euler114
    xcount3_0 bientôt au point

--- euler/src/euler/EulerMainNoScalaTest.scala
+++ euler/src/euler/EulerMainNoScalaTest.scala
@@ -44,7 +44,7 @@ object EulerMainNoScalaTest extends App {
 }
 
 class Euler114 {
-    new DoZeJob1(15)
+    new DoZeJob1(18)
 
     class DoZeJob1(len: Int) {
         val lcheck = Map(1 -> 1, 2 -> 1, 3 -> 2, 4 -> 4, 5 -> 7, 6 -> 11, 7 -> 17, 8 -> 27, 9 -> 43, 10 -> 67, 11 -> 102)
@@ -89,10 +89,20 @@ class Euler114 {
                         val x = y.filter(_.toList.sliding(2).toList.filter(_.count(_ != '0') > 1).isEmpty).toList.sorted
                         z._1 match {
                             case 2 => myAssert2(x.length, xCount0_2)
+                            case 3 => {
+                                val w = x.groupBy(_.length).map(c => (c._2.head.length, c._2.length)).toList.sortBy(_._2)
+                                myPrintDln("x count0[" + z._1 + "] ", x.length, w.mkString("\n  ", "\n  ", "\n  "))
+                                val rcount3 = w.map(c => {
+                                    val v = xCount0_3(c._1)
+                                    myAssert2(v, c._2)
+                                    v
+                                }).sum
+
+                            }
                             case _ => {
                                 //myPrintDln("y count0[" + z._1 + "] ", y.toList.length, y.toList.sorted)
                                 //val w = x.groupBy(_.toList.map(_.toInt).sum).map(c => (c._1, c._2.head, c._2.length)).toList.sortBy(_._3)
-                                val w = x.groupBy(_.length).map(c => (c._2.length, c._2)).toList.sortBy(_._1)
+                                val w = x.groupBy(_.length).map(c => (c._2.length, c._2.head.length, c._2)).toList.sortBy(_._1)
                                 myPrintDln("x count0[" + z._1 + "] ", x.length, w.mkString("\n  ", "\n  ", "\n  "))
                                 //myPrintDln("x count0[" + z._1 + "] ", x.length)
                             }
@@ -108,9 +118,35 @@ class Euler114 {
             myPrintDln(__FUNC__)
             (7 to len).map(i => (i - 6) * (i - 6)).sum
         }
-        def xCount0_3 = {
-            myPrintDln(__FUNC__)
-            (7 to len).map(i => (i - 6) * (i - 6)).sum
+        def xCount0_3(slen: Int) = {
+            var lres = 0
+            if ((len > 10) & (slen == 5)) {
+                lres += 3
+            } else {
+                lres += 1
+            }
+            if ((len > 12) & (slen == 7)) {
+                lres += 30
+            } else {
+                lres += 10
+            }
+            if ((len > 13) & (slen == 8)) {
+                lres += 60
+            } else {
+                lres += 20
+            }
+            if ((len > 14) & (slen == 9)) {
+                lres += 105
+            } else {
+                lres += 35
+            }
+            if ((len > 15) & (slen == 10)) {
+                lres += 168
+            } else {
+                lres += 56
+            }
+            myPrintDln(__FUNC__, len, slen, lres)
+            lres
         }
 
         def ubersetz(i: Int) = vubersetz.substring(i, i + 1)
