6bee3f934b8deb0a06331ab1cd806846c897f4da
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2013-11-18 15:19:33 +0100

    Euler132 on réessaye

--- euler/src/euler/EulerMainNoScalaTest.scala
+++ euler/src/euler/EulerMainNoScalaTest.scala
@@ -29,7 +29,7 @@ object EulerMainNoScalaTest extends App {
         var t_start = Calendar.getInstance
         myPrintDln("Hello World!")
 
-        new Euler429
+        new Euler132
 
         timeStamp(t_start, "Au revoir Monde!")
     } catch {
@@ -45,31 +45,13 @@ object EulerMainNoScalaTest extends App {
     }
 }
 
-class Euler429 {
-    val modulo = BigInt(1000000009)
+class Euler132 {
+    (8 until 67 by 8).map(doZeJob1(_))
 
-    myPrintIt(modulo, new EulerDiv(modulo).primes)
-    (2 until 100).map(doZeJob1(_))
-
-    def doZeJob1(i: Int) = {
-        val fbi = EulerFactorielle.fact2(i)
-        val primes = new EulerDiv(fbi).primes
-        val byPrime = primes.groupBy(_ + 0).map(_._2.product).toList.sorted
-        val prod = products(byPrime)
-        val res = result(i, prod)
-        //myPrintln(i, res)
-        res
-    }
-
-    def products(byPrime: List[BigInt]) = {
-        val len = byPrime.length - 1
-        ((1 to len).map(byPrime.combinations(_).toList.map(_.product)).flatten
-            ++ List[BigInt](1, byPrime.product)).sorted.toList
-    }
-    def result(i: Int, products: List[BigInt]) = {
-        val sumod = products.map(bi => bi * bi).sum % modulo
-        myPrintln("  " + i + " " + sumod + " " +
-            new EulerDiv(sumod).primes.groupBy(_ + 0).map(c => (c._1, c._2.length)).toList.sortBy(_._1))
-        sumod
+    def doZeJob1(n: Int) {
+        val bi = BigInt((1 until n + 1).map(z => "1").mkString)
+        val primes = new EulerDiv(bi).primes
+        myPrintln(n, bi, primes)
     }
 }
+
--- euler/src/euler/MainNoScalatestPasEncoreReussi.scala
+++ euler/src/euler/MainNoScalatestPasEncoreReussi.scala
@@ -279,3 +279,32 @@ class Euler241 {
     def perfquot(bi: BigInt, sbi: BigInt) = sbi.toDouble / bi.toDouble
 }
 
+class Euler429 {
+    val modulo = BigInt(1000000009)
+
+    myPrintIt(modulo, new EulerDiv(modulo).primes)
+    (2 until 100).map(doZeJob1(_))
+
+    def doZeJob1(i: Int) = {
+        val fbi = EulerFactorielle.fact2(i)
+        val primes = new EulerDiv(fbi).primes
+        val byPrime = primes.groupBy(_ + 0).map(_._2.product).toList.sorted
+        val prod = products(byPrime)
+        val res = result(i, prod)
+        //myPrintln(i, res)
+        res
+    }
+
+    def products(byPrime: List[BigInt]) = {
+        val len = byPrime.length - 1
+        ((1 to len).map(byPrime.combinations(_).toList.map(_.product)).flatten
+            ++ List[BigInt](1, byPrime.product)).sorted.toList
+    }
+    def result(i: Int, products: List[BigInt]) = {
+        val sumod = products.map(bi => bi * bi).sum % modulo
+        myPrintln("  " + i + " " + sumod + " " +
+            new EulerDiv(sumod).primes.groupBy(_ + 0).map(c => (c._1, c._2.length)).toList.sortBy(_._1))
+        sumod
+    }
+}
+
