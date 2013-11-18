fba50357114e48d2a0add899df8172e64cd408cb
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2013-11-18 17:10:09 +0100

    Euler132 mfmf

--- euler/src/euler/EulerMainNoScalaTest.scala
+++ euler/src/euler/EulerMainNoScalaTest.scala
@@ -46,12 +46,41 @@ object EulerMainNoScalaTest extends App {
 }
 
 class Euler132 {
-    (8 until 67 by 8).map(doZeJob1(_))
+    val premiers = (new CheckEulerPrime(20000000, 1000)).premiers
+    myPrintIt(1000000000, new EulerDiv2(1000000000, premiers).primes)
 
-    def doZeJob1(n: Int) {
+    // 5169354 (10000) dernier 1378001
+    // List(11, 17, 41, 73, 101, 137, 251, 257, 271, 353, 401, 449, 641, 751, 1201, 1409, 1601, 3541, 4001, 5051, 9091, 10753, 15361, 16001, 19841, 21001, 21401, 24001, 25601, 27961, 60101, 69857, 76001, 160001, 162251, 453377, 670001, 952001, 976193, 1378001)
+    // 1822662 (100000) dernier 544001
+    doZeJob1(10)
+    /*doZeJob2(2, 11)
+    doZeJob2(5, 6)
+    doZeJob2(10, 5)*/
+    var z = ListSet[BigInt]()
+    z = z ++ doZeJob3(2, 10)
+    MyLog.waiting(1 second)
+    z = z ++ doZeJob3(5, 5)
+    MyLog.waiting(1 second)
+    z = z ++ doZeJob3(10, 5)
+    val y = z.toList.sorted
+    myErrPrintln(z)
+    myErrPrintln(y, y.length)
+    myErrPrintln(y.take(40))
+    myErrPrintln(y.take(40).sum)
+
+    def doZeJob1(n: Int) = {
         val bi = BigInt((1 until n + 1).map(z => "1").mkString)
-        val primes = new EulerDiv(bi).primes
-        myPrintln(n, bi, primes)
+        val primes = new EulerDiv2(bi, premiers).primes
+        val justChecking = primes.product
+        myPrintln("\n\n\n", n, primes, primes.length, primes.sum,
+            justChecking.toString.toList.length, bi / justChecking, "\n\n\n")
+        primes
+    }
+    def doZeJob2(n: Int, limit: Int) = {
+        (2 until limit).toList.map(i => doZeJob1(Math.pow(n, i).toInt))
+    }
+    def doZeJob3(n: Int, limit: Int) = {
+        doZeJob1(Math.pow(n, limit).toInt)
     }
 }
 
