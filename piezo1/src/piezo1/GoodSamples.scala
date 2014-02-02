d8d1bb912aa292f830997adf633bdd5c9606b46e
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2014-02-02 09:07:42 +0100

    Piezo1: 1st draft of ZeCurve (la courbe moyennée)

--- piezo1/src/piezo1/GoodSamples.scala
+++ piezo1/src/piezo1/GoodSamples.scala
@@ -1,5 +1,26 @@
 package piezo1
 
-class GoodSamples(l: List[(Double, Int)],goodSampleRoots: List[(Int,Int)]) {
+import kebra._
+import kebra.MyLog._
+
+class GoodSamples(l: List[(Double, Int)], goodSampleRoots: List[(Int, Int)]) {
+    val goodSamples = goodSampleRoots.map(z => l.slice(z._1, z._1 + z._2))
+    val sampleMax = goodSampleRoots.map(_._2).max.toDouble
+
+    var zeCurve = (0 to sampleMax.toInt).map((0.0, _))
+    goodSamples.take(2).foreach(z => {
+        var queue = z.map(_._1).zipWithIndex.take(6)
+        val ratio = z.size / sampleMax
+        myPrintIt(z.size, sampleMax, "%3.2f".format(ratio), queue)
+        zeCurve = zeCurve.take(5).map(pt => {
+            queue = queue.dropWhile(y => {
+                //myPrintln(y, y._2 * ratio, pt)
+                y._2 * ratio <= pt._2
+            })
+            //myPrintIt(queue.head, pt, zeCurve)
+            (pt._1 + queue.head._1, pt._2)
+        })
+        myPrintIt(zeCurve.take(7))
+    })
 
 }
--- piezo1/src/piezo1/Piezo1Main.scala
+++ piezo1/src/piezo1/Piezo1Main.scala
@@ -7,28 +7,37 @@ import _root_.scalaSci.math.plot.plotTypes._
 import _root_.scalaSci.math.plot.plot._
 import _root_.scalaSci.math.plot.canvas._
 import java.awt.Color
+import scala.concurrent.duration.Duration
 
 object Piezo1Main extends App {
-  val zorgl = (0 to 10).map(z => FFT.Complex(z)).toList
-  myPrintIt(FFT.filtre(zorgl, 2, 7))
+    val zorgl = (0 to 10).map(z => FFT.Complex(z)).toList
+    myPrintIt(FFT.filtre(zorgl, 2, 7))
 
-  var hue = 0.1.toFloat
-  val incHue = 1.0.toFloat / 5
+    var hue = 0.1.toFloat
+    val incHue = 1.0.toFloat / 5
 
-  val pz = new Piezo3NoGraphic
-  val fig1 = figure()
-  title("Hello World1!")
-  myPrintln("  fig(" + fig1 + ")")
-  draw(pz.ld50k._1, "start")
+    //val pz = new Piezo3NoGraphic
+    val fig1 = figure()
+    title("Hello World1!")
+    myPrintln("  fig(" + fig1 + ")")
+    /*draw(pz.ld50k._1, "start")
   draw(pz.filtered, "pulses")
   draw(pz.iffted3, "iffted3")
-  draw(pz.iffted11.map(z => (z._1 * 10.0, z._2)), "iffted11")
+  draw(pz.iffted11.map(z => (z._1 * 10.0, z._2)), "iffted11")*/
 
-  def draw(l: List[(Double, Int)], name: String) {
-    val y = l.map(_._1)
-    val x = l.map(_._2 * pz.periodSample)
-    plot(x.toArray, y.toArray, getColor(hue), name)
-    hue += incHue
-  }
+    val pz4 = new Piezo4NoGraphic
+
+    draw(pz4.ldecimed.map(c => (c._1, c._2 * pz4.ttick)), "decimed")
+    draw(pz4.iffted8.map(c => ((c._1 * 4).toDouble, (c._2 * pz4.decimation * pz4.ttick))), "filtered1")
+    draw(pz4.iffted9.map(c => (((c._1 * 4) + 150).toDouble, c._2 * pz4.decimation * pz4.ttick)), "filtered2")
+    scatterPlotsOn
+    draw(pz4.pics.goodSampleRoots.map(c => (c._2.toDouble/10.0, c._1 * pz4.ttick)), "goodSampleRoots")
+
+    def draw(l: List[(Double, Duration)], name: String) {
+        val y = l.map(_._1)
+        val x = l.map(_._2.toMillis.toDouble)
+        plot(x.toArray, y.toArray, getColor(hue), name)
+        hue += incHue
+    }
 
 }
