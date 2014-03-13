89a24b5ac506a2508e4557374915962c46763332
Eric Mariacher <eric.mariacher@gmail.com>
2014-03-13 12:15:32 +0100

    some changes + treemap

--- bugstats/src/bugstats/BugStatsMain.scala
+++ bugstats/src/bugstats/BugStatsMain.scala
@@ -9,47 +9,60 @@ import kebra._
 import kebra.MyLog
 import kebra.MyFileChooser
 import language.postfixOps
+import java.text.SimpleDateFormat
 
 object BugStatsMain extends App {
-	val f = (new MyFileChooser("GetBugsLog")).justChooseFile("xml");
-	val L = MyLog.newMyLog(this.getClass.getName,f,"htm")
+    val f = (new MyFileChooser("GetBugsLog")).justChooseFile("xml");
 
-			val bugs = (new TTPro).ParseXml(f)
-			//println(bugs.mkString("\n"))
+    val bugs = (new TTPro).ParseXml(f)
+    //println(bugs.mkString("\n"))
 
-			val today = Calendar.getInstance
-			val latestActivityInInputFile = bugs.map(_.h.last.c).sorted.last
-			L.myPrintln("latestActivityInInputFile: "+printZisday(latestActivityInInputFile,"dMMMyyyy"))
-			var endDate = today 
-			if((now is latestActivityInInputFile).before(Today minus 3 days)) {
-				//L.createGui(List(("endDate dd/MM/yy",printZisday(latestActivityInInputFile,"dd/MM/yy"),new TextField)))
-				L.createGui(new ZeParameters(List((
-						"endDate dd/MM/yy",new MyParameter(printZisday(latestActivityInInputFile,"dd/MM/yy"),
-								printZisday(latestActivityInInputFile,"dd/MM/yy"), new TextField)
-						))))
-						val z = L.Gui.getAndClose.head._2.value
-						endDate = MyLog.ParseDate(z,"dd/MM/yy")
-			}
-	endDate.clear(Calendar.HOUR)
-	L.myPrintln("\nendDate: "+printZisday(endDate,"dMMMyyyy"))
+    val today = Calendar.getInstance
+    val latestActivityInInputFile = bugs.map(_.h.last.c).sorted.last
+    myPrintln("latestActivityInInputFile: "+printZisday(latestActivityInInputFile, "dMMMyyyy"))
+    var endDate = today
+    if ((now is latestActivityInInputFile).before(Today minus 3 days)) {
+        /*val L = MyLog.newMyLog(this.getClass.getName, f, "htm")
+        //L.createGui(List(("endDate dd/MM/yy",printZisday(latestActivityInInputFile,"dd/MM/yy"),new TextField)))
+        L.createGui(new ZeParameters(List((
+            "endDate dd/MM/yy", new MyParameter(printZisday(latestActivityInInputFile, "dd/MM/yy"),
+                printZisday(latestActivityInInputFile, "dd/MM/yy"), new TextField)))))
+        val z = L.Gui.getAndClose.head._2.value
+        endDate = MyLog.ParseDate(z, "dd/MM/yy")*/
+        endDate = latestActivityInInputFile
+    }
 
-	val projets = bugs.map(_.product).distinct.map(new Projet(_,bugs,endDate))	
+    endDate.clear(Calendar.HOUR)
+    myPrintln("\nendDate: "+printZisday(endDate, "dMMMyyyy"))
 
-	L.myHErrPrintln("<html><head>"+ChartsGoogle.htmlHeaderJustZeGraphs+"\n<title>"+printZisday(endDate,"dMMMyyyy")+" bug trends</title></head><body>\n")
+    val projets = bugs.map(_.product).distinct.map(new Projet(_, bugs, endDate))
 
-	L.myHErrPrintln("\n<h2>Active Projects at "+printZisday(endDate,"dMMMyyyy")+"</h2>\n"+ChartsGoogle.columnChart("ActiveProjects",
-			projets.filter(_.isAlive).map(p => (p.p,p.rangees.last.r)),
-			Projet.rangeeMaitresse.map(c => c._1+" "+c._2)))
+    var s = "<html><head>"+ChartsGoogle.htmlHeaderJustZeGraphs+"\n<title>"+printZisday(endDate, "dMMMyyyy")+" bug trends</title></head><body>\n"
 
-			L.myHErrPrintln("\n<h2>Word Cloud</h2>\n"+ChartsGoogle.wordCloud(bugs.map(b => (b.product,b.summary)),"Word Cloud"))
+    s += "\n<h2>Active Projects at "+printZisday(endDate, "dMMMyyyy")+"</h2>\n"+ChartsGoogle.columnChart("ActiveProjects",
+        projets.filter(_.isAlive).map(p => (p.p, p.rangees.last.r)),
+        Projet.rangeeMaitresse.map(c => c._1+" "+c._2))
 
-			L.myHErrPrintln(projets.map(p => { "\n<h3>"+p.p+"</h3>\n"+
-					ChartsGoogle.stackedCurve(p.p, p.rangees.map(r => {
-						(r.c2string,r.r)
-					}), Projet.rangeeMaitresse.map(c => c._1+" "+c._2))
-			}).mkString(""))
+    s += "\n<h2>Word Cloud</h2>\n"+ChartsGoogle.wordCloud(bugs.map(b => (b.product, b.summary)), "Word Cloud")
 
-			L.myHErrPrintln("</body></html>")
+    s += projets.map(p => {
+        "\n<h3>"+p.p+"</h3>\n"+
+            ChartsGoogle.stackedCurve(p.p, p.rangees.map(r => {
+                (r.c2string, r.r)
+            }), Projet.rangeeMaitresse.map(c => c._1+" "+c._2))
+    }).mkString("")
 
-			L.hcloseFiles(L.working_directory + File.separatorChar + "htmlheader.html", logPostProcess.htmlPostProcess)
+    s += "</body></html>"
+
+    toFileAndDisplay("tests_status_"+new SimpleDateFormat("ddMMMyy_HH_mm").format(Calendar.getInstance.getTime)+".html", s)
+
+    var sjson = "{ \"name\": \"flare\", \"children\":\n"
+    val projectsbyType = projets.groupBy(p => p.p.substring(0, 2))
+    myPrintDln(projectsbyType.map(z => z._1+" "+z._2.size))
+    sjson += projectsbyType.map(z => {
+        "{\"name\": \""+z._1+"\", \"children\":\n"+z._2.map(p => p.json(Today)).mkString("  [\n    ",",\n    ","\n  ]\n  }")
+    }).mkString("[\n  ",",\n  ","\n]\n}")
+    copy2File("flare.json",sjson)
+    display(new File("treemap.html"))
+
 }
