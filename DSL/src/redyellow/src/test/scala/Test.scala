e2bcf84ab4b71aded785a1bb4ede0ccaabb03d82
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2015-05-20 17:56:35 +0200

    DSL redyellow: going further with tests

--- DSL/src/redyellow/src/test/scala/Test.scala
+++ DSL/src/redyellow/src/test/scala/Test.scala
@@ -18,6 +18,14 @@ class Test extends FunSuite {
       ).p.toString should equal("RedList(1,2) threshold: 2, GreenLists(List(3, 4) - List(5, 6)), ")
     (Rule red List(1, 2) threshold 2 next nand green List(3, 4) next nand green List(5, 6) next nand
       ).p.toString should equal("RedList(1,2) threshold: 2, GreenLists(List(3, 4) - List(5, 6)), ")
+    (Rule red List(1, 2) threshold 2 next nand red List(7, 8) threshold 7
+      ).p.toString should equal("RedList(1,2,7,8) threshold: 7, ")
+    (Rule red List(1, 2) threshold 2 yellow List(7, 8) threshold 7
+      ).p.toString should equal("RedList(1,2) threshold: 2, YellowList(7,8) threshold: 7, ")
+    (Rule red List(1, 2) threshold 2 green List(3, 4) yellow List(7, 8) threshold 7 green List(5, 6) red List(9, 10) threshold 5
+      ).p.toString should equal("RedList(1,2,9,10) threshold: 5, YellowList(7,8) threshold: 7, GreenLists(List(3, 4) - List(5, 6)), ")
+    (Rule red List(1, 2) threshold 2 next nand red List(1, 2) threshold 7
+      ).p.toString should equal("RedList(1,2,1,2) threshold: 7, ")
   }
 
   test("Test error cases") {
