f42846f2bfb5f6b336e7acb9a749d24f4a2a5fb8
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2015-05-30 07:03:12 +0200

    euler243: quicker stuff

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -8,9 +8,14 @@ class EulerMain extends FlatSpec with Matchers {
       val primes = new EulerDiv(d).primes
       val divisors = new EulerDivisors(primes).divisors
       val resil = Range(1, d, 2).filter(i => {
-        divisors.filter(div => i % div == 0).isEmpty
+        //divisors.filter(div => i % div == 0).isEmpty
+        divisors.find(div => i % div == 0) match {
+          case Some(i) => false
+          case _ => true
+        }
       })
-      println(d+"\t"+resil.length+"/"+(d - 1)+"\t"+(1.0 * resil.length / (d - 1))+"\t"+primes+"\t"+new EulerDiv(resil.length).primes)
+      val pgcd_primes = new EulerDiv(resil.length).primes intersect primes
+      println(d + "\t" + resil.length + "/" + (d - 1) + "\t" + (1.0 * resil.length / (d - 1)) + "\t" + primes + "\t" + new EulerDiv(resil.length).primes + "\t" + pgcd_primes)
       (resil.length, d - 1)
     }
 
@@ -23,8 +28,10 @@ class EulerMain extends FlatSpec with Matchers {
     resilience(2 * 2 * 3 * premiers.take(5).product.toInt)
     resilience(2 * 3 * 5 * premiers.take(5).product.toInt)
     resilience(2 * 2 * 3 * 3 * premiers.take(5).product.toInt)
+    resilience(premiers.take(5).product.toInt)
     resilience(premiers.take(6).product.toInt)
     resilience(premiers.take(7).product.toInt)
+    resilience(premiers.take(8).product.toInt)
 
     println(1.0 * 15499 / 94744)
 
