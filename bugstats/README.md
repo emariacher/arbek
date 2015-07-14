8dcc6d553ce771aba18d483ba0eede416c03740d
Eric Mariacher <eric.mariacher@gmail.com>
2015-07-14 20:06:55 +0200

    Bugstats: cosmetic

--- bugstats/README.md
+++ bugstats/README.md
@@ -2,3 +2,8 @@ Eric Mariacher trademark 1oct2012
 snapshot: open bugs status on various projects
 word cloud: on bug appearing in bug summaries
 bug trends: on various projects
+
+bugstats.ReqStatsMainEta
+bugstats.BugStatsMainEta
+bugstats.BugStatsMainLogitech
+
--- bugstats/core/src/main/scala/BugStatsMain.scala
+++ bugstats/core/src/main/scala/BugStatsMain.scala
@@ -1,5 +1,5 @@
 package bugstats
 
-object BugStatsMainReqEta extends App { new DoZeJob(new TTProReqEta) }
+object ReqStatsMainEta extends App { new DoZeJob(new TTProReqEta) }
 object BugStatsMainEta extends App { new DoZeJob(new TTProEta) }
 object BugStatsMainLogitech extends App { new DoZeJob(new TTProLogitech) }
