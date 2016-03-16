9f29730c9875e480c35b4428478b8cab438dd784
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-03-16 14:05:39 +0100

    Labyrinthe: some factorization

--- fourmis/src/main/scala/LabyrintheMainApp.scala
+++ fourmis/src/main/scala/LabyrintheMainApp.scala
@@ -1,57 +1,6 @@
 package labyrinthe
 
-import scala.swing.MainFrame
-import scala.swing.Slider
-import scala.swing.Button
-import scala.swing.Label
-import scala.swing.BoxPanel
-import scala.swing.event.ValueChanged
-import scala.swing.event.ButtonClicked
-import scala.swing.Orientation
-import scala.swing.Swing
-import scala.swing.SimpleSwingApplication
-import labyrinthe.Tableaux._
-import labyrinthe.PanelType._
-import labyrinthe.LL._
-import java.io.File
-import kebra.MyLog._
-
-object LabyrintheMainApp extends SimpleSwingApplication {
-    newMyLL(this.getClass.getName, new File("out\\cowabunga"), "htm", true)
-
-    def top = new MainFrame {
-        title = "Sors du Labyrinthe. Vert va toujours a tribord, Rouge toujours a babord et bleu est fou."
-        val sliderpp = new Slider {
-            majorTickSpacing = 100
-            minorTickSpacing = 10
-            paintLabels = true
-            paintTicks = true
-        }
-        val buttonStep = new Button { text = "step" }
-        val label = new Label { text = "Idle Label" }
-
-        //val tableau = new ZePanel(label, new RowCol(40,40))
-
-        ZePanel.newZePanel(label, new RowCol(40, 40), PanelType.LABY)
-
-        contents = new BoxPanel(Orientation.Vertical) {
-            contents += label
-            contents += new BoxPanel(Orientation.Horizontal) {
-                contents ++= tbx.lj.map(_.label)
-            }
-            contents += ZePanel.zp
-            contents += new BoxPanel(Orientation.Horizontal) {
-                contents += sliderpp
-                contents += buttonStep
-            }
-            border = Swing.EmptyBorder(30, 30, 10, 30)
-        }
-        listenTo(sliderpp, buttonStep)
-        reactions += {
-            case ValueChanged(b)  => ZePanel.za ! ("slider", sliderpp.value)
-            case ButtonClicked(b) => ZePanel.za ! b.text
-            case _                =>
-        }
-    }
-
-}
+/**
+ * Created by ericm_000 on 16.03.2016.
+ */
+object LabyrintheMainApp extends UI
---
+++ fourmis/src/main/scala/UI.scala
@@ -0,0 +1,53 @@
+package labyrinthe
+import java.io.File
+
+import labyrinthe.LL._
+import labyrinthe.Tableaux._
+
+import scala.swing._
+import scala.swing.event.{ButtonClicked, ValueChanged}
+
+/**
+ * Created by ericm_000 on 16.03.2016.
+ */
+class UI extends SimpleSwingApplication {
+    val titre = "Sors du Labyrinthe. Vert va toujours a tribord, Rouge toujours a babord et bleu est fou."
+    val buttonStepLbl = "step"
+    val labelLbl = "Idle Label"
+    val zpanelType = PanelType.LABY
+    newMyLL(this.getClass.getName, new File("out\\cowabunga"), "htm", true)
+
+    def top = new MainFrame {
+        title = titre
+        val sliderpp = new Slider {
+            majorTickSpacing = 100
+            minorTickSpacing = 10
+            paintLabels = true
+            paintTicks = true
+        }
+        val buttonStep = new Button { text = buttonStepLbl }
+        val label = new Label { text = labelLbl }
+
+        ZePanel.newZePanel(label, new RowCol(40, 40), zpanelType)
+
+        contents = new BoxPanel(Orientation.Vertical) {
+            contents += label
+            contents += new BoxPanel(Orientation.Horizontal) {
+                contents ++= tbx.lj.map(_.label)
+            }
+            contents += ZePanel.zp
+            contents += new BoxPanel(Orientation.Horizontal) {
+                contents += sliderpp
+                contents += buttonStep
+            }
+            border = Swing.EmptyBorder(30, 30, 10, 30)
+        }
+        listenTo(sliderpp, buttonStep)
+        reactions += {
+            case ValueChanged(b)  => ZePanel.za ! ("slider", sliderpp.value)
+            case ButtonClicked(b) => ZePanel.za ! b.text
+            case _                =>
+        }
+    }
+
+}
