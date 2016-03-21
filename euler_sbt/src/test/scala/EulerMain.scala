f25abf14b18ebc0576b9b6327635ae4527c78832
Eric Mariacher <eric.mariacher@gmail.com>
2016-03-21 22:26:55 +0100

    Euler500 ongoing

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -1,42 +1,45 @@
 import org.scalatest._
 
 class EulerMain extends FlatSpec with Matchers {
-  "Euler243" should "be OK" in {
-    println("Euler243")
+  "Euler500" should "be OK" in {
+    println("Euler500")
     val premiers = EulerPrime.premiers1000
-    def resilience(d: Int) = {
-      val primes = new EulerDiv(d).primes
-      val divisors = new EulerDivisors(primes).divisors
-      val resil = Range(1, d, 2).filter(i => {
-        //divisors.filter(div => i % div == 0).isEmpty
-        divisors.find(div => i % div == 0) match {
-          case Some(i) => false
-          case _ => true
-        }
-      })
-      val pgcd_primes = new EulerDiv(resil.length).primes intersect primes
-      println(d + "\t" + resil.length + "/" + (d - 1) + "\t" + (1.0 * resil.length / (d - 1)) + "\t" + primes + "\t" + new EulerDiv(resil.length).primes + "\t" + pgcd_primes)
-      (resil.length, d - 1)
+    def numdiv(d: BigInt) = {
+      val divisors = new EulerDivisors(new EulerDiv(d)).divisors
+      println(d + "\t" + (divisors.length+2) + "\t" + ispowerof2(divisors.length+2) + "\t" + divisors)
     }
 
-    resilience(12) should be ===(4, 11)
-    resilience(premiers.take(4).product.toInt)
-    resilience(premiers.take(5).product.toInt)
-    resilience(2 * premiers.take(5).product.toInt)
-    resilience(2 * 2 * premiers.take(5).product.toInt)
-    resilience(2 * 3 * premiers.take(5).product.toInt)
-    resilience(2 * 2 * 3 * premiers.take(5).product.toInt)
-    resilience(2 * 3 * 5 * premiers.take(5).product.toInt)
-    resilience(2 * 2 * 3 * 3 * premiers.take(5).product.toInt)
-    resilience(premiers.take(5).product.toInt)
-    resilience(premiers.take(6).product.toInt)
-    resilience(premiers.take(7).product.toInt)
-    resilience(premiers.take(8).product.toInt)
+    def ispowerof2(d: BigInt) = {
+      val divisorsDistinct = new EulerDivisors(new EulerDiv(d)).primesUnique.toList.distinct
+      divisorsDistinct.length == 1 & divisorsDistinct.head == 2
+    }
 
-    println(1.0 * 15499 / 94744)
+    numdiv(2)
+    numdiv(4)
+    numdiv(8)
+    numdiv(12)
+    numdiv(16)
+    numdiv(24)
+    numdiv(32)
+    numdiv(48)
+    numdiv(64)
+    numdiv(96)
+    numdiv(120)
+    numdiv(120*2)
+    numdiv(120*3)
+    numdiv(120*5)
+    numdiv(120*2*3)
+    numdiv(120*7)
+    numdiv(120*11)
+    numdiv(120*5*2)
+    numdiv(120*5*3)
+    numdiv(120*5*7)
+    numdiv(120*7*2)
+    numdiv(120*7*3)
+    numdiv(120*7*11)
 
     val result = 0
-    println("Euler243[" + result + "]")
+    println("Euler500[" + result + "]")
     result should be === 0
   }
 }
---
+++ euler_sbt/src/test/scala/EulerNotYetSolved.scala
@@ -0,0 +1,45 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by emariacher on 21.03.2016.
+ */
+class EulerNotYetSolved extends FlatSpec with Matchers {
+  "Euler243" should "be OK" in {
+    println("Euler243")
+    val premiers = EulerPrime.premiers1000
+    def resilience(d: Int) = {
+      val primes = new EulerDiv(d).primes
+      val divisors = new EulerDivisors(primes).divisors
+      val resil = Range(1, d, 2).filter(i => {
+        //divisors.filter(div => i % div == 0).isEmpty
+        divisors.find(div => i % div == 0) match {
+          case Some(i) => false
+          case _ => true
+        }
+      })
+      val pgcd_primes = new EulerDiv(resil.length).primes intersect primes
+      println(d + "\t" + resil.length + "/" + (d - 1) + "\t" + (1.0 * resil.length / (d - 1)) + "\t" + primes + "\t" + new EulerDiv(resil.length).primes + "\t" + pgcd_primes)
+      (resil.length, d - 1)
+    }
+
+    resilience(12) should be ===(4, 11)
+    resilience(premiers.take(4).product.toInt)
+    resilience(premiers.take(5).product.toInt)
+    resilience(2 * premiers.take(5).product.toInt)
+    resilience(2 * 2 * premiers.take(5).product.toInt)
+    resilience(2 * 3 * premiers.take(5).product.toInt)
+    resilience(2 * 2 * 3 * premiers.take(5).product.toInt)
+    resilience(2 * 3 * 5 * premiers.take(5).product.toInt)
+    resilience(2 * 2 * 3 * 3 * premiers.take(5).product.toInt)
+    resilience(premiers.take(5).product.toInt)
+    resilience(premiers.take(6).product.toInt)
+    resilience(premiers.take(7).product.toInt)
+    resilience(premiers.take(8).product.toInt)
+
+    println(1.0 * 15499 / 94744)
+
+    val result = 0
+    println("Euler243[" + result + "]")
+    result should be === 0
+  }
+}
