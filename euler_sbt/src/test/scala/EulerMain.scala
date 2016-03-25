4741661372f831744a3feefa3badcc87a621f9d1
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-03-25 18:45:02 +0100

    Euler27 Solved

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -12,28 +12,37 @@ class EulerMain extends FlatSpec with Matchers {
       ((n + a) * n) + b
     }
 
-    def rangeFrom(a: Int, b: Int): Stream[Int] = a #:: rangeFrom(b,1 + b)
-    def getprimesFrom(a: Int, b: Int): List[(Int,Int)] = {
+    def rangeFrom(a: Int, b: Int): Stream[Int] = a #:: rangeFrom(b, 1 + b)
+    def getprimesFrom(a: Int, b: Int): List[(Int, Int)] = {
       val r = rangeFrom(0, 1)
-      val z = r.map(n => (n,quadratic(a, b, n))).takeWhile(q => {
+      val z = r.map(n => (n, quadratic(a, b, n))).takeWhile(q => {
         premiers.contains(q._2)
       }).toList
       //println("     "+a + " " + b + " " + z)
       z
     }
 
-    var z = rangeFrom(0, 1).take(7)
-    println(z.toList)
+    var un_a_linfini = rangeFrom(0, 1).take(7)
+    println(un_a_linfini.toList)
 
-    (-5 to 5).map(a => {
-      (-5 to 5).map(b => {
-        println(a+" "+b+" "+getprimesFrom(a,b))
+    val z = (-1000 to 1000).map(a => {
+      //EulerPrime.premiers10000.filter(_ < 1602).filter(b => b + a > 0).map(b => {
+        EulerPrime.premiers1000.filter(b => b + a > 0).map(b => {
+        val gp = getprimesFrom(a, b.toInt)
+        //println(a + " " + b + " " + gp)
+        (a, b.toInt, gp.length, gp)
       })
-    })
+    }).flatten.filter(_._3 > 1).sortBy(_._3)
+    println(z.mkString("\n  ", "\n  ", "\n  "))
+    println(EulerPrime.premiers1000)
+    println(EulerPrime.premiers1000.last)
+    println(EulerPrime.premiers10000.filter(_ < 1602).last)
+    val gp = getprimesFrom(-79, 1601)
+    println(-79,1601,gp.length, gp)
 
-
-    val result = 0
+    val y = z.last
+    val result = y._1*y._2
     println("Euler27[" + result + "]")
-    result should be === 0
+    result should be === (-61*971)
   }
 }
