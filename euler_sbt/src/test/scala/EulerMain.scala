ef592fb04d997f5578f235a53598ca93410eee22
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-03-25 17:46:01 +0100

    Euler27: draft

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -1,45 +1,39 @@
 import org.scalatest._
+import Stream.cons
 
 class EulerMain extends FlatSpec with Matchers {
-  "Euler500" should "be OK" in {
-    println("Euler500")
-    val premiers = EulerPrime.premiers1000
-    def numdiv(d: BigInt) = {
-      val divisors = new EulerDivisors(new EulerDiv(d)).divisors
-      println(d + "\t" + (divisors.length+2) + "\t" + ispowerof2(divisors.length+2) + "\t" + divisors)
+  "Euler27" should "be OK" in {
+    println("Euler27")
+
+    val premiers = EulerPrime.premiers100000
+    val max = 10
+
+    def quadratic(a: Int, b: Int, n: Int) = {
+      ((n + a) * n) + b
     }
 
-    def ispowerof2(d: BigInt) = {
-      val divisorsDistinct = new EulerDivisors(new EulerDiv(d)).primesUnique.toList.distinct
-      divisorsDistinct.length == 1 & divisorsDistinct.head == 2
+    def rangeFrom(a: Int, b: Int): Stream[Int] = a #:: rangeFrom(b,1 + b)
+    def getprimesFrom(a: Int, b: Int): List[(Int,Int)] = {
+      val r = rangeFrom(0, 1)
+      val z = r.map(n => (n,quadratic(a, b, n))).takeWhile(q => {
+        premiers.contains(q._2)
+      }).toList
+      //println("     "+a + " " + b + " " + z)
+      z
     }
 
-    numdiv(2)
-    numdiv(4)
-    numdiv(8)
-    numdiv(12)
-    numdiv(16)
-    numdiv(24)
-    numdiv(32)
-    numdiv(48)
-    numdiv(64)
-    numdiv(96)
-    numdiv(120)
-    numdiv(120*2)
-    numdiv(120*3)
-    numdiv(120*5)
-    numdiv(120*2*3)
-    numdiv(120*7)
-    numdiv(120*11)
-    numdiv(120*5*2)
-    numdiv(120*5*3)
-    numdiv(120*5*7)
-    numdiv(120*7*2)
-    numdiv(120*7*3)
-    numdiv(120*7*11)
+    var z = rangeFrom(0, 1).take(7)
+    println(z.toList)
+
+    (-5 to 5).map(a => {
+      (-5 to 5).map(b => {
+        println(a+" "+b+" "+getprimesFrom(a,b))
+      })
+    })
+
 
     val result = 0
-    println("Euler500[" + result + "]")
+    println("Euler27[" + result + "]")
     result should be === 0
   }
 }
--- euler_sbt/src/test/scala/EulerNotYetSolved.scala
+++ euler_sbt/src/test/scala/EulerNotYetSolved.scala
@@ -42,4 +42,48 @@ class EulerNotYetSolved extends FlatSpec with Matchers {
     println("Euler243[" + result + "]")
     result should be === 0
   }
+
+  "Euler500" should "be OK" in {
+    println("Euler500")
+    val premiers = EulerPrime.premiers1000
+    def numdiv(d: BigInt) = {
+      val divisors = new EulerDivisors(new EulerDiv(d)).divisors
+      println(d + "\t" + (divisors.length+2) + "\t" + ispowerof2(divisors.length+2) + "\t" + divisors)
+    }
+
+    def ispowerof2(d: BigInt) = {
+      val divisorsDistinct = new EulerDivisors(new EulerDiv(d)).primesUnique.toList.distinct
+      divisorsDistinct.length == 1 & divisorsDistinct.head == 2
+    }
+
+    numdiv(2)
+    numdiv(4)
+    numdiv(8)
+    numdiv(12)
+    numdiv(16)
+    numdiv(24)
+    numdiv(32)
+    numdiv(48)
+    numdiv(64)
+    numdiv(96)
+    numdiv(120)
+    numdiv(120*2)
+    numdiv(120*3)
+    numdiv(120*5)
+    numdiv(120*2*3)
+    numdiv(120*7)
+    numdiv(120*11)
+    numdiv(120*5*2)
+    numdiv(120*5*3)
+    numdiv(120*5*7)
+    numdiv(120*7*2)
+    numdiv(120*7*3)
+    numdiv(120*7*11)
+
+    val result = 0
+    println("Euler500[" + result + "]")
+    result should be === 0
+  }
+
+
 }
