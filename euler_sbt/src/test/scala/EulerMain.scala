57226e6811a3da8d79f0a65e92dbf5c5554080ad
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-03-27 19:57:45 +0200

    Euler100: ongoing

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -7,24 +7,27 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler100" should "be OK" in {
     println("Euler100")
 
-    val limit = 1000
+    val limit = 1000000
 
     def is50pourcent(total: Double): (Boolean, String) = {
       val totcar = math.pow(total - 0.5, 2)
       val totsqrt = math.sqrt(totcar / 2)
       val blue = math.ceil(totsqrt)
       val stat = (blue / total) * ((blue - 1) / (total - 1))
-      if (math.abs(stat - 0.5) < 0.0000000001) {
+      if (math.abs(stat - 0.5) < 0.00000001) {
         val totalInt = BigDecimal(total).setScale(0, BigDecimal.RoundingMode.HALF_UP).toBigInt
         val blueInt = BigDecimal(blue).setScale(0, BigDecimal.RoundingMode.HALF_UP).toBigInt
         val statInt = (blueInt * (blueInt - 1)) * 2 == (totalInt * (totalInt - 1))
-        (true, (total, totcar, totsqrt, blueInt, stat, (totalInt * (totalInt - 1)), (blueInt * (blueInt - 1)), "-----------------------------").toString)
+        (statInt, (totalInt, blueInt, stat, new EulerDiv(totalInt).primes, new EulerDiv(totalInt-1).primes,
+          new EulerDiv(blueInt).primes, new EulerDiv(blueInt-1).primes, "-----------------------------").toString)
       } else {
         (false, (total, totcar, totsqrt, blue, stat).toString)
       }
     }
 
+    val t_ici = timeStamp(t_start, "ici!")
     val z1 = (1 to limit).map(b => is50pourcent(b.toDouble)).filter(_._1)
+    timeStamp(t_ici, "la!")
 
     println(z1.mkString("\n  ", "\n  ", "\n  "))
 
