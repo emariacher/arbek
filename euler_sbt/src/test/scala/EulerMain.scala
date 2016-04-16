b42c590f8ff27f8b8a056e617834ababde91be74
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-04-16 22:37:10 +0200

    Euler451

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -12,10 +12,14 @@ class EulerMain extends FlatSpec with Matchers {
 
     def coprimes(i: Int): (Int, List[BigInt], List[Int]) = {
       val primesi = new EulerDiv(i).primes
-      (i, primesi, (1 until i).toList.filter(u => {
-        val primesu = new EulerDiv(u).primes
-        primesi.intersect(primesu).length == 0
-      }))
+      if (primesi.length == 1) {
+        (i, primesi, (1 until i).toList)
+      } else {
+        (i, primesi, (1 until i).toList.filter(u => {
+          val primesu = new EulerDiv(u).primes
+          primesi.intersect(primesu).length == 0
+        }))
+      }
     }
 
     def modinv(i: Int, j: Int) = {
@@ -35,6 +39,13 @@ class EulerMain extends FlatSpec with Matchers {
       val cp = coprimes(i)
       val md = cp._3.dropRight(1).map(j => modinv(i, j))
       val md1 = md.reverse.filter(u => u._1 == u._2)
+      /*if (md1.head._1 > 1) {
+        if (!EulerPrime.isPrime(md1.head._1)) {
+          println(i, md1.head._1, "*******************************************")
+        } else {
+          println(i, md1.head._1, "++++++")
+        }
+      }*/
       (i, md1.head._1, md1.filter(_._1 != 1))
     }
 
@@ -57,14 +68,41 @@ class EulerMain extends FlatSpec with Matchers {
     doZeJob(15)._2 should be === 11
     doZeJob(100)._2 should be === 51
     doZeJob(7)._2 should be === 1
-    val limit = 1000
+    val powerlimit = 8
+    val pow2 = (2 to powerlimit).map(Euler.powl(2, _).toInt)
+    val pow3 = (2 to powerlimit).map(Euler.powl(3, _).toInt)
+    val limit = Euler.powl(2, powerlimit).toInt
     val t_ici = timeStamp(t_start, "ici!")
-    val z = (3 to limit).toList.map(doZeJob(_))
     val t_la = timeStamp(t_start, "la!")
     val z2 = (3 to limit).toList.map(doZeJob2(_))
     val t_la2 = timeStamp(t_la, "la2!")
-    z2.map(u => (u._1, u._2)) should be === z.map(u => (u._1, u._2))
-    println(z.mkString("\n  ", "\n  ", "\n  "))
+    if (powerlimit < 9) {
+      val z = (3 to limit).toList.map(doZeJob(_))
+      z2.map(u => (u._1, u._2)) should be === z.map(u => (u._1, u._2))
+      println(z.mkString("\n  ", "\n  ", "\n  "))
+      println(z.filter(u => {pow2.contains(u._1)}).mkString("\n  ", "\n  ", "\n  "))
+      println(z.filter(u => {pow3.contains(u._1)}).mkString("\n  ", "\n  ", "\n  "))
+    }
+
+    //var cpts = (0 to limit).map(i => (i, z2.map(_._2).count(_ == i))).grouped(16)
+    //println(cpts.mkString("\n  ", "\n  ", "\n  "))
+
+
+    val r = (2 to powerlimit).map(plmt => {
+      val lmt = Euler.powl(2, plmt).toInt
+      val rg = z2.take(lmt - 2)
+      val s = rg.map(_._2).sum
+      (plmt, lmt, s, rg.count(_._2 == 1))
+    })
+    println(r.mkString("\n  ", "\n  ", "\n  "))
+    println(r.sliding(2).map(c => {
+      (c.head._1, c.last._1,
+        c.last._3.toDouble / c.head._3.toDouble,
+        c.last._4.toDouble / c.head._4.toDouble)
+    }).mkString("\n  ", "\n  ", "\n  "))
+
+
+
 
     var result = 0
     println("Euler451[" + result + "]")
