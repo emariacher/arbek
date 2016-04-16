e0a95a5d086727b4ab90f9e705bccd19b77db9de
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-04-16 09:19:49 +0200

    Euler451: 1ère simplification

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -22,10 +22,27 @@ class EulerMain extends FlatSpec with Matchers {
       (j, (1 to i - 1).toList.dropWhile(u => u * j % i != 1).head)
     }
 
+    def modinv2(i: Int, j: Int) = {
+      val z = (i / 2 to i - 1).toList.dropWhile(u => u * j % i != 1)
+      if (z.isEmpty) {
+        (j, 1)
+      } else {
+        (j, z.head)
+      }
+    }
+
     def doZeJob(i: Int) = {
       val cp = coprimes(i)
       val md = cp._3.dropRight(1).map(j => modinv(i, j))
-      (cp, md.reverse.filter(u => u._1 == u._2).head._1, md)
+      val md1 = md.reverse.filter(u => u._1 == u._2)
+      (i, md1.head._1, md1.filter(_._1 != 1))
+    }
+
+    def doZeJob2(i: Int) = {
+      val cp = coprimes(i)
+      val md = cp._3.dropRight(1).map(j => modinv2(i, j))
+      val md1 = md.reverse.filter(u => u._1 == u._2)
+      (i, md1.head._1, md1.filter(_._1 != 1))
     }
 
     println(doZeJob(15))
@@ -35,7 +52,14 @@ class EulerMain extends FlatSpec with Matchers {
     doZeJob(15)._2 should be === 11
     doZeJob(100)._2 should be === 51
     doZeJob(7)._2 should be === 1
-
+    val limit = 1000
+    val t_ici = timeStamp(t_start, "ici!")
+    val z = (3 to limit).toList.map(doZeJob(_))
+    val t_la = timeStamp(t_start, "la!")
+    val z2 = (3 to limit).toList.map(doZeJob2(_))
+    val t_la2 = timeStamp(t_la, "la2!")
+    z2.map(u => (u._1, u._2)) should be === z.map(u => (u._1, u._2))
+    println(z.mkString("\n  ", "\n  ", "\n  "))
 
     var result = 0
     println("Euler451[" + result + "]")
