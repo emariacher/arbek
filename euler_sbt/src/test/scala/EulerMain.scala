2b86b682722f561cd1253cb6e867e5af84c64f7f
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-05-05 06:51:22 +0200

    Euler518 :(

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -12,40 +12,103 @@ class EulerMain extends FlatSpec with Matchers {
 
     def Yes(a: BigInt, b: BigInt, c: BigInt) = {
       var r = EulerPrime.isPrime(a) && EulerPrime.isPrime(b) && EulerPrime.isPrime(c) && (b + 1).toDouble / (a + 1).toDouble == (c + 1).toDouble / (b + 1).toDouble
-      //println((a, b, c), (a + 1, b + 1, c + 1), (b + 1) / (a + 1), r)
+      //if(r) println("           ", (a, b, c), (a + 1, b + 1, c + 1), (b + 1) / (a + 1), r)
       r
     }
 
-    def Yes2(l: List[BigInt]) = Yes(l.head, l.apply(1), l.last)
+    def Yes2(l: List[BigInt]) = Yes(l.sorted.head, l.sorted.apply(1), l.sorted.last)
 
     def S(n: Int) = {
-      val t_iciS = timeStamp(t_start, "ici! S(" + n + ")")
-      val z = EulerPrime.premiers10000.filter(_ < n).toList.combinations(3).filter(Yes2(_)).toList
-      println("S(" + n + ")", z.mkString("\n  ", "\n  ", "\n  "), z.map(_.sum), z.flatten.sum)
+      val t_iciS = timeStamp(t_start, "")
+      val p = EulerPrime.premiers10000.filter(_ < n).toList
+      val z = p.combinations(3).filter(Yes2(_)).toList
+      println("S(" + n + ")", p.length, z.length, z.mkString("\n  ", " - ", "\n  "), z.map(_.sum).sorted, z.flatten.sum)
       val t_laS = timeStamp(t_iciS, "la! S(" + n + ")")
       z.flatten.sum
     }
 
     def S2(n: Int, i: Int) = {
-      val t_iciS = timeStamp(t_start, "ici! S2(" + n + ")")
+      val t_iciS = timeStamp(t_start, "")
       val p = EulerPrime.premiers10000.filter(_ < n).toList
       val y = p.sliding(i).map(slide => slide.combinations(3).filter(Yes2(_)).toList).flatten.toList
-      val z = ListSet.empty[List[BigInt]] ++ y
+      val z = (ListSet.empty[List[BigInt]] ++ y).toList.sortBy(_.apply(1)).sortBy(_.head)
       //println("S2 y(" + n + "," + i + ")", y.mkString("\n  ", "\n  ", "\n  "), y.map(_.sum), y.flatten.sum)
-      println("S2(" + n + "," + i + ")", p.length, z.mkString("\n  ", "\n  ", "\n  "), z.map(_.sum), z.map(_.sum).sum)
+      println("S2(" + n + "," + i + ")", p.length, z.length, z.mkString("\n  ", " - ", "\n  "), z.map(_.sum), z.map(_.sum).sum)
       val t_laS = timeStamp(t_iciS, "la! S2(" + n + ")")
       z.map(_.sum).sum
     }
 
+    def T(n: Int) = {
+      val p = EulerPrime.premiers10000.filter(_ < n).toList
+      val y = p.combinations(3).filter(Yes2(_)).toList.flatten
+      val z = (ListSet.empty[BigInt] ++ y).toList.sorted
+      println("T(" + n + ")", p.length, z.length, "\n" + z, "\n" + p)
+      z
+    }
+
+    def S3(n: Int) = {
+      val t_iciS = timeStamp(t_start, "")
+      val prems = EulerPrime.premiers10000.filter(_ < n).toList
+      var pg = ListSet[BigInt](2)
+      var z: BigInt = 0
+      var l = ListSet.empty[List[BigInt]]
+      prems.foreach(p => {
+        if (pg.contains(p)) {
+          val x = prems.toList.combinations(2).toList.filter(c => Yes2(List(p, c.head, c.last))).map(c => {
+            List(p, c.head, c.last).sorted
+          })
+          l = l ++ x
+          val y = (ListSet.empty[BigInt] ++ x.flatten).toList.sorted
+          val u = x.map(c => c.sum)
+          pg = pg ++ y
+          //println("*1*", p, x, y, u, pg.toList.sorted, "\n  ", l)
+        } else {
+          val x = pg.toList.combinations(2).toList.filter(c => Yes2(List(p, c.head, c.last))).map(c => {
+            List(p, c.head, c.last).sorted
+          })
+          l = l ++ x
+          val y = (ListSet.empty[BigInt] ++ x.flatten).toList.sorted
+          val u = x.map(c => c.sum)
+          pg = pg ++ y
+          //println("*2*", p, x, y, u, pg.toList.sorted, "\n  ", l)
+          if (pg.contains(p)) {
+            val x = prems.toList.combinations(2).toList.filter(c => Yes2(List(p, c.head, c.last))).map(c => {
+              List(p, c.head, c.last).sorted
+            })
+            l = l ++ x
+            val y = (ListSet.empty[BigInt] ++ x.flatten).toList.sorted
+            val u = x.map(c => c.sum)
+            pg = pg ++ y
+            //println("*3*", p, x, y, u, pg.toList.sorted, "\n  ", l)
+          }
+        }
+      })
+      z = l.toList.flatten.sum
+      val t_laS = timeStamp(t_iciS, "la! S3(" + n + ")")
+      println(pg.toList.sorted)
+      println("S3(" + n + ")", prems.length, l.toList.length, l.toList.sortBy(_.apply(1)).sortBy(_.head).mkString("\n  ", " - ", "\n  "), l.toList.map(_.sum).sorted, l.toList.flatten.sum)
+      z
+    }
+
+
+    Yes(37, 151, 607) should be === true
     Yes(2, 5, 11) should be === true
     Yes(2, 5, 13) should be === false
     Yes(31, 47, 71) should be === true
     Yes(31, 53, 71) should be === false
     Yes(2, 6, 11) should be === false
     Yes(5, 2, 11) should be === false
+    Yes2(List(5, 2, 11)) should be === true
     println("********************************")
     S(100) should be === 1035
     S2(100, 19) should be === 1035
+    S(200) should be === S2(200, 43)
+    T(100)
+    T(200)
+    S(60) should be === S3(60)
+    S(50) should be === S3(50)
+    S(200) should be === S3(200)
+    S(1000) should be === S3(1000)
 
     var result = 0
     println("Euler518[" + result + "]")
