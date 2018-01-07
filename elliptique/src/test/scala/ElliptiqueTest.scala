a3d3cfc154d832ad3479ffe7e3955dd09698d4bd
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2018-01-07 14:52:57 +0100

    elliptique starting

--- elliptique/src/test/scala/ElliptiqueTest.scala
+++ elliptique/src/test/scala/ElliptiqueTest.scala
@@ -5,13 +5,24 @@ import scala.collection.immutable.{Range, ListSet}
 import scala.math.BigInt
 
 class ElliptiqueTest extends FlatSpec with Matchers {
-  "Elliptique27" should "be OK" in {
-    println("Euler27")
+  def rangeStream2(a: BigInt, b: BigInt): Stream[BigInt] = (a*a) #:: rangeStream2(b, 1 + b)
+
+  def stream_zero_a_linfini2: Stream[BigInt] = rangeStream2(0, 1)
+
+  def rangeStream3(a: BigInt, b: BigInt): Stream[BigInt] = ((a*a*a)+7) #:: rangeStream3(b, 1 + b)
+
+  def stream_zero_a_linfini3: Stream[BigInt] = rangeStream3(0, 1)
+
+  "Elliptique" should "be OK" in {
+    println("Elliptique")
     val t_ici = timeStamp(t_start, "ici!")
+    stream_zero_a_linfini2 take 5 foreach println
+    stream_zero_a_linfini3 take 5 foreach println
+
     val t_la = timeStamp(t_ici, "la! ******************************")
 
     val result = 0
-    println("Elliptique27[" + result + "]")
+    println("Elliptique[" + result + "]")
     result should be === 0
   }
 }
