ef0efe4baa40fbd49921659970a781217875a166
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2018-05-25 17:48:39 +0200

    LinkedIn utility

---
+++ code/javaref/social_networking/getemailpattern/Getemailpattern.java
@@ -0,0 +1,207 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.*;
+import javax.swing.*;
+
+/**
+ *  Description of the Class
+ *
+ *@author     Eric Mariacher
+ *@created    September 14, 2005
+ */
+class Getemailpattern extends Thread {
+
+	String binaryFile = null;
+
+	String currep = null;
+	List defectIn = new ArrayList();
+	List defectOut = new ArrayList();
+
+	MesFichiers f = new MesFichiers();
+	List files = new ArrayList();
+
+	String s_buildNumber = null;
+	String working_directory = null;
+	File defvardiff;
+	FileOutputStream outputf;
+
+
+
+	/**
+	 *  Constructor for the BuildRelease2 object
+	 *
+	 *@param  zt             Description of the Parameter
+	 *@exception  Exception  Description of the Exception
+	 */
+	public Getemailpattern(GetemailpatternHMI zt) throws Exception {
+		this.zt = zt;
+		this.err = zt.err;
+		initLogFiles();
+		start();
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	void closeLogFiles() throws Exception {
+		f.cleanfos(logfile);
+		f.cleanfos(errfile);
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  s              Description of the Parameter
+	 *@return                Description of the Return Value
+	 *@exception  Exception  Description of the Exception
+	 */
+	String crunchwebpage(String s) throws Exception {
+		myErrPrintln("crunchwebpage");
+		String slocal = new String(s.toLowerCase().replaceAll("<[/]*strong>", ""));
+		slocal = new String(slocal.replaceAll("[\\s]*\\.[\\s]*", ".").replaceAll("[\\s|\\p{Punct}]+dot[\\s|\\p{Punct}]+", "."));
+		slocal = new String(slocal.replaceAll("[\\s|\\p{Punct}]+at[\\s|\\p{Punct}]+", "@").replaceAll("[\\s|\\p{Punct}]+@[\\s|\\p{Punct}]+", "@"));
+		slocal = new String(slocal.replaceAll("[\\s|\\p{Punct}]+a[\\s|\\p{Punct}]+", "@"));
+		Pattern p = Pattern.compile("[\\w|\\.|\\-|\\_]+\\@[\\w|\\.]+");
+		Matcher m = p.matcher(slocal);
+		myPrintln(s);
+		myPrintln("\n***********\n************\n*********\n*********\n" + slocal);
+		StringBuffer sb = new StringBuffer();
+		while (m.find()) {
+			String email = new String(m.group().replaceAll("\\.com\\.\\.\\.", "\\.com").replaceAll("\\.\\.\\.", "\\."));
+			if (m.group().lastIndexOf(".") > m.group().lastIndexOf("@")) {
+				email=email.substring(email.lastIndexOf("@"));
+				myErrPrintln("mail: " + email);
+				if ((sb.indexOf(email) < 0)&&(zt.alreadyInvited.indexOf(email) < 0)) {
+					sb.append(email + ", ");
+				}
+			}
+		}
+		return sb.toString();
+	}
+
+
+	JTextArea err;
+	int errfile;
+
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	void initLogFiles() throws Exception {
+		Calendar c = Calendar.getInstance();
+		Date date = c.getTime();
+		SimpleDateFormat datefmt = new SimpleDateFormat("ddMMMyy_HH_mm");
+		SimpleDateFormat datefmt2 = new SimpleDateFormat("ddMMMyy");
+		String sdate = new String(datefmt.format(date));
+		logfile = f.addFichier("logfile_" + sdate + ".log");
+		errfile = f.addFichier("errfile_" + sdate + ".log");
+	}
+
+
+	int logfile;
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  s              Description of the Parameter
+	 *@exception  Exception  Description of the Exception
+	 */
+	void myErrPrint(String s) throws Exception {
+		if (err != null) {
+			err.append(s);
+			f.writeFile(errfile, s);
+			myPrint(s);
+			f.writeFile(logfile, s);
+		}
+		System.out.print(s);
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  s              Description of the Parameter
+	 *@exception  Exception  Description of the Exception
+	 */
+	void myErrPrintln(String s) throws Exception {
+		myErrPrint(s + "\n");
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  s              Description of the Parameter
+	 *@exception  Exception  Description of the Exception
+	 */
+	void myPrint(String s) throws Exception {
+		f.writeFile(logfile, s);
+		System.out.print(s);
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  s              Description of the Parameter
+	 *@exception  Exception  Description of the Exception
+	 */
+	void myPrintln(String s) throws Exception {
+		myPrint(s + "\n");
+	}
+
+
+	/**
+	 *  Description of the Method
+	 */
+	public void run() {
+		try {
+			myErrPrintln("running");
+			myPrintln("yo1[" + zt.alreadyInvited + "]");
+			String emailist = new String(crunchwebpage(zt.webpage.getText()));
+			zt.email_list.setText(emailist);
+			myErrPrintln("runned");
+			zt.alreadyInvited = zt.alreadyInvited.concat(emailist);
+			outputf = new FileOutputStream(zt.defvardiff);
+			outputf.write((new String(zt.alreadyInvited + "\n")).getBytes());
+			outputf.write((new String("\n")).getBytes());
+			outputf.close();
+			closeLogFiles();
+		} catch (Exception e) {
+			zt.AnalyzeLabel.setText("*******Errors: not Built!********");
+			try {
+				PipedInputStream piErr = new PipedInputStream();
+				PipedOutputStream poErr = new PipedOutputStream(piErr);
+				System.setErr(new PrintStream(poErr, true));
+				e.printStackTrace();
+				myErrPrintln(e.toString());
+				int len = 0;
+				byte[] buf = new byte[1024];
+				while (true) {
+					len = piErr.read(buf);
+					if (len == -1) {
+						break;
+					}
+					err.append(new String(buf, 0, len));
+				}
+			} catch (Exception ze) {}
+		} finally {}
+	}
+
+
+	GetemailpatternHMI zt;
+
+}
+// -- end class BuildRelease2
+
---
+++ code/javaref/social_networking/linkedin2/Linkedin2HMI.java
@@ -0,0 +1,202 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+
+/**
+ *  Description of the Class
+ *
+ *@author     Eric Mariacher
+ *@created    September 14, 2005
+ */
+public class Linkedin2HMI extends JPanel implements ActionListener, DefVar {
+	JTextPane webpage = new JTextPane();
+	JTextPane email_list = new JTextPane();
+	JFrame buildFrame;
+	JPanel buildPanel;
+	JLabel AnalyzeLabel;
+	JButton jbAnalyze;
+	JTextArea err;
+	BufferedReader inputf;
+	String server;
+	Linkedin2 br;
+	String alreadyInvited;
+	File defvardiff;
+
+
+
+	/**
+	 *  Constructor for the SeapineBuildReleaseTool object
+	 */
+	public Linkedin2HMI() {
+		super(new BorderLayout());
+		//Create and set up the window.
+		buildFrame = new JFrame("linkedin2");
+		buildFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		buildFrame.setSize(new Dimension(200, 700));
+
+		//Create and set up the panel.
+		buildPanel = new JPanel(new GridLayout(5, 2));
+		//Create the log first, because the action listeners
+		//need to refer to it.
+		err = new JTextArea(5, 25);
+		err.setMargin(new Insets(1, 1, 1, 1));
+		err.setEditable(false);
+		JScrollPane errScrollPane = new JScrollPane(err);
+		err.setForeground(Color.red);
+
+		//Add the widgets.
+		addWidgets();
+
+		//Set the default button.
+		buildFrame.getRootPane().setDefaultButton(jbAnalyze);
+
+		//Add the panel to the window.
+		buildFrame.getContentPane().setLayout(new BoxLayout(buildFrame.getContentPane(), BoxLayout.PAGE_AXIS));
+		buildFrame.getContentPane().add(buildPanel);
+		buildMyPanel(buildFrame, webpage, new String("Web Page"));
+		buildMyPanel(buildFrame, email_list, new String("email list"));
+		buildFrame.getContentPane().add(errScrollPane);
+		//Display the window.
+		buildFrame.pack();
+		//buildFrame.setSize(buildFrame.getPreferredSize());
+		buildFrame.setVisible(true);
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  tp     Description of the Parameter
+	 *@param  titre  Description of the Parameter
+	 *@param  bf     Description of the Parameter
+	 */
+	void buildMyPanel(JFrame bf, JTextPane tp, String titre) {
+		JPanel mp = new JPanel();
+		mp.setLayout(new BoxLayout(mp, BoxLayout.PAGE_AXIS));
+		JLabel label = new JLabel(titre);
+		mp.add(label);
+		tp.setMargin(new Insets(1, 1, 1, 1));
+		tp.setEditable(true);
+		tp.setContentType("text/html");
+		JScrollPane sp = new JScrollPane(tp);
+		sp.setPreferredSize(new Dimension(200, 100));
+		sp.setMinimumSize(new Dimension(200, 50));
+		mp.add(sp);
+		bf.getContentPane().add(mp);
+	}
+
+
+
+	/**
+	 *  Create and add the widgets.
+	 */
+	private void addWidgets() {
+		Font bigFont = new Font(null, Font.BOLD, 16);
+		//Create widgets.
+
+		jbAnalyze = new JButton("Analyze!");
+		jbAnalyze.addActionListener(this);
+		AnalyzeLabel = new JLabel("not analyzed yet", SwingConstants.LEFT);
+
+		buildPanel.add(jbAnalyze);
+		buildPanel.add(AnalyzeLabel);
+		//jbAnalyze.setEnabled(false);
+		AnalyzeLabel.setEnabled(false);
+		AnalyzeLabel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		//read default variables file
+		try {
+			defvardiff = new File("LinkedIn2");
+			if (defvardiff.canRead()) {
+				err.append("ya1[" + defvardiff.getName() + "]");
+				inputf = new BufferedReader(new FileReader(defvardiff));
+				String linef;
+				int cpt = 0;
+				alreadyInvited = new String("gna");
+				while ((linef = inputf.readLine()) != null) {
+					err.append("ya2[" + cpt + ":" + linef + "], ");
+					alreadyInvited = alreadyInvited.concat(linef);
+					err.append("ya3[" + alreadyInvited + "], ");
+					cpt++;
+				}
+				inputf.close();
+			}
+		} catch (FileNotFoundException e) {
+			err.append("             *warning* no File with default values found! expected at[" +
+					defvardiff.getName() + "] continuing...");
+		} catch (Exception e) {
+			/*
+			 *  try {
+			 *  PipedInputStream piErr   = new PipedInputStream();
+			 *  PipedOutputStream poErr  = new PipedOutputStream(piErr);
+			 *  System.setErr(new PrintStream(poErr, true));
+			 *  e.printStackTrace();
+			 *  /myErrPrintln(e.toString());
+			 *  int len                  = 0;
+			 *  byte[] buf               = new byte[1024];
+			 *  while(true) {
+			 *  len = piErr.read(buf);
+			 *  if(len == -1) {
+			 *  break;
+			 *  }
+			 *  err.append(new String(buf, 0, len));
+			 *  }
+			 *  } catch(Exception ze) {}
+			 */
+			System.err.println("*1*" + e);
+		} finally {
+
+		}
+
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  event  Description of the Parameter
+	 */
+	public void actionPerformed(ActionEvent event) {
+		//log.append(event.toString() + "$$$$$$$$$$");
+		if (event.getSource() == jbAnalyze) {
+			try {
+				AnalyzeLabel.setText("Analyzing!");
+				br = new Linkedin2(this);
+			} catch (Exception e) {
+				System.err.println("*2*" + e);
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+	/**
+	 *  Create the GUI and show it. For thread safety, this method should be
+	 *  invoked from the event-dispatching thread.
+	 */
+	private static void createAndShowGUI() {
+		//Make sure we have nice window decorations.
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		Linkedin2HMI build = new Linkedin2HMI();
+	}
+
+
+	/**
+	 *  The main program for the SeapineBuildReleaseTool class
+	 *
+	 *@param  args  The command line arguments
+	 */
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(
+			new Runnable() {
+				public void run() {
+					createAndShowGUI();
+				}
+			});
+	}
+}
+
