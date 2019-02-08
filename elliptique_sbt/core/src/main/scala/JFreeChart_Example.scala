1972328e1d0c8a0f97b91c48209ca5d8bcfc3be8
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2019-02-08 22:50:37 +0100

    Trois courbes

--- elliptique_sbt/core/src/main/scala/JFreeChart_Example.scala
+++ elliptique_sbt/core/src/main/scala/JFreeChart_Example.scala
@@ -19,26 +19,25 @@ object JFreeChart_Example extends App with scalax.chart.module.Charting {
 
   myPrintDln(e.title)
   val lp = e.curve
-  val plot = XYLineChart(lp).plot
-  plot.setRenderer(new XYLineAndShapeRenderer(false, true))
 
-  val chartScatter = XYChart(plot, title = e.title, legend = true)(theme = ChartTheme.Default)
+  def ScatterPlot(title: String, ll: List[(String, List[(BigInt, BigInt)])]) = {
+    val XYSeriesCollection = new XYSeriesCollection()
+    ll.foreach(l => {
+      val allPoints = new XYSeries(l._1)
+      l._2.foreach(p => allPoints.add(p._1.toInt, p._2.toInt))
+      XYSeriesCollection.addSeries(allPoints)
+    })
 
-  chartScatter.show(e.title, (800, 800))
+    val plot = XYLineChart(XYSeriesCollection).plot
+    plot.setRenderer(new XYLineAndShapeRenderer(false, true))
 
-  val XYSeriesCollection1 = new XYSeriesCollection()
-  val allPoints1st = new XYSeries(e.title)
-  lp.foreach(p => allPoints1st.add(p._1.toInt, p._2.toInt))
-  XYSeriesCollection1.addSeries(allPoints1st)
-  val allPoints2nd = new XYSeries(e.title + " subset")
-  lp.take(10).foreach(p => allPoints2nd.add(p._1.toInt + 1, p._2.toInt + 1))
-  XYSeriesCollection1.addSeries(allPoints2nd)
+    val chartScatter = XYChart(plot, title = e.title, legend = true)(theme = ChartTheme.Default)
 
-  val plot2 = XYLineChart(XYSeriesCollection1).plot
-  plot2.setRenderer(new XYLineAndShapeRenderer(false, true))
+    chartScatter.show(title, (800, 800))
+  }
 
-  val chartScatter2 = XYChart(plot2, title = e.title, legend = true)(theme = ChartTheme.Default)
-
-  chartScatter2.show(e.title + " 2", (800, 800))
+  ScatterPlot("Zobi la mouche", List((e.title, lp),
+    ("zorglub", (for (i <- 1 to 5) yield (BigInt(i), BigInt(i))).toList),
+    ("bulgroz", (for (i <- 1 to 23) yield (BigInt(i+1)*2, BigInt(i-1)*2)).toList)))
 
 }
