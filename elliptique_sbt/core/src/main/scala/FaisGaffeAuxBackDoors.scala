dcb3a8d51aa00395c2b0f97dfffa7e9e9283b9c3
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2019-02-10 17:05:37 +0100

    elliptique_sbt backdoors 1ère version qui pourrait tenir la route

--- elliptique_sbt/core/src/main/scala/FaisGaffeAuxBackDoors.scala
+++ elliptique_sbt/core/src/main/scala/FaisGaffeAuxBackDoors.scala
@@ -12,26 +12,25 @@ object FaisGaffeAuxBackDoorsackdoors extends App with scalax.chart.module.Charti
 
   def analyseModulo(e: Elliptique, title: String) = {
     myPrintDln(title)
-    val lZeros = e.curve.filter(p => p._1 * p._2 == 0)
 
-    if (!lZeros.isEmpty) {
-      val ls1 = e.loopsum(lZeros.head)
-      myPrintIt(lZeros.size, ls1)
+    var lsp = List[MySeries]()
+    val lZeros = e.curve.filter(p => p._1 * p._2 == 0).map(z => {
+      val lsz = e.loopsum(z)
+      myPrintDln(lsz)
+      lsp = lsp :+ new MySeries("loopsum(" + z + ")", false, true, lsz._5)
+      lsz._5
+    }).flatten.distinct
 
-      ScalaChartUtils.LineScatterPlot(title, List(
-        new MySeries(e.title, false, true, e.curved.filter(p => ls1._5.find(_ == p).isEmpty)),
-        new MySeries("loopsum(" + ls1._2 + ")", false, true, ls1._5)
-      ))
-    }
+    lsp = lsp :+ new MySeries(e.title, false, true, e.curved.filter(p => lZeros.find(_ == p).isEmpty))
+    ScalaChartUtils.LineScatterPlot(title, lsp)
   }
 
-
-  /*myPrintDln("****Choisis la bonne courbe****")
-  myPrintDln(" **Montre une bonne courbe**")
-  myPrintDln(" **Montre une mauvaise courbe**")*/
-  myPrintDln("****Choisis le bon modulo****")
-  analyseModulo(new Elliptique(67, 0, 7), " **Montre un bon modulo**")
-  analyseModulo(new Elliptique(71, 0, 7), " **Montre un mauvais modulo**")
-  analyseModulo(new Elliptique(73, 0, 7), " **Montre un mauvais modulo**")
-  analyseModulo(new Elliptique(17, 0, 7), " **Montre un mauvais modulo**")
+/*myPrintDln("****Choisis la bonne courbe****")
+myPrintDln(" **Montre une bonne courbe**")
+myPrintDln(" **Montre une mauvaise courbe**")*/
+myPrintDln("****Choisis le bon modulo****")
+analyseModulo(new Elliptique(67, 0, 7), " **Montre un bon modulo**")
+analyseModulo(new Elliptique(71, 0, 7), " **Montre un mauvais modulo**")
+analyseModulo(new Elliptique(73, 0, 7), " **Montre un mauvais modulo**")
+analyseModulo(new Elliptique(17, 0, 7), " **Montre un mauvais modulo**")
 }
