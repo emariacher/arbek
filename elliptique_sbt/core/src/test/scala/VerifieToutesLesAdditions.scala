a12918445a7d80b10f17ddc3b5118ce79c72c7e5
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2019-02-11 21:55:30 +0100

    elliptique_sbt demain je le montre sur une courbe!

--- elliptique_sbt/core/src/test/scala/VerifieToutesLesAdditions.scala
+++ elliptique_sbt/core/src/test/scala/VerifieToutesLesAdditions.scala
@@ -13,20 +13,39 @@ https://fr.wikipedia.org/wiki/Courbe_elliptique
 class VerifieToutesLesAdditions extends FlatSpec with Matchers {
   val rnd = new Random(0)
 
-  def ParcoursToutesLesPossibilitesDAddition(e: Elliptique): Unit = {
+  def ParcoursToutesLesPossibilitesDAddition(e: Elliptique): List[(BigInt, BigInt)] = {
     myPrintDln(e.title)
     e.curve.combinations(2).toList.foreach(pq => {
       //print(pq.head + " + " + pq.last + " = " + e.plus(pq.head, pq.last) + ", ")
 
       if (!e.lZeros.find(e.plus(pq.head, pq.last) == _).isEmpty) {
-        myErrPrintDln("\n  " + e.lZeros + " contient " + pq.head + " + " + pq.last + " = " + e.plus(pq.head, pq.last))
+        myErrPrintDln(e.lZeros + " contient " + pq.head + " + " + pq.last + " = " + e.plus(pq.head, pq.last))
       }
     })
+    e.lZeros
   }
 
   "CheckToutesLesAdditions" should "be OK" in {
-    ParcoursToutesLesPossibilitesDAddition(new Elliptique(67, 0, 7))
-    ParcoursToutesLesPossibilitesDAddition(new Elliptique(17, 0, 7))
+    ParcoursToutesLesPossibilitesDAddition(new Elliptique(67, 0, 7)).isEmpty shouldEqual true
+    ParcoursToutesLesPossibilitesDAddition(new Elliptique(17, 0, 7)).isEmpty shouldEqual false
+  }
+
+  "Check17" should "be OK" in {
+    val e = new Elliptique(17, 0, 7)
+    myPrintDln(e.title, e.curve)
+    e.plus((BigInt(1), BigInt(5)), (BigInt(15), BigInt(4))) shouldEqual e.lZeros.head
+    e.plus(e.lZeros.head, e.lZeros.head) shouldEqual e.ZERO
+    myPrintIt(e.loopsum(e.curve.head))
+    myPrintIt(e.curve.filter(p => (e.loopsum(e.curve.head)._4 :+ e.curve.head :+ e.plus(e.curve.head, e.curve.head)).find(p == _).isEmpty))
+    e.curve.filter(p => (e.loopsum(e.curve.head)._4 :+ e.curve.head :+ e.plus(e.curve.head, e.curve.head)).find(p == _).isEmpty).isEmpty shouldEqual false
+  }
+
+  "Check67" should "be OK" in {
+    val e = new Elliptique(67, 0, 7)
+    myPrintDln(e.title)
+    myPrintIt(e.curve.head, e.plus(e.curve.head, e.curve.head))
+    myPrintIt(e.curve.filter(p => (e.loopsum(e.curve.head)._4 :+ e.curve.head :+ e.plus(e.curve.head, e.curve.head)).find(p == _).isEmpty))
+    e.curve.filter(p => (e.loopsum(e.curve.head)._4 :+ e.curve.head :+ e.plus(e.curve.head, e.curve.head)).find(p == _).isEmpty).isEmpty shouldEqual true
   }
 
 }
