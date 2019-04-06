cbc495d7ccec01f2c5739177578b682901d2090f
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2019-04-06 08:58:17 +0200

    ScalaGraphlayout fignolage intermédiaire

--- ScalaGraphLayout/core/src/main/scala/Edge.scala
+++ ScalaGraphLayout/core/src/main/scala/Edge.scala
@@ -2,14 +2,14 @@ package graphlayout
 
 import java.awt.{Color, Graphics2D}
 
-import graphlayout.Tableaux.tbx
 import kebra.MyLog
 
 class Edge(val from: GNode, val to: GNode) {
   var len = .0
   var diff = .0
+  var dist = .0
 
-  override def toString: String = from.lbl + (from.x.toInt, from.y.toInt).toString + "->" + to.lbl + (to.x.toInt, to.y.toInt).toString + "[%.2f".format(diff) +"/%.2f]".format(len)
+  override def toString: String = from.lbl + (from.x.toInt, from.y.toInt).toString + "->" + to.lbl + (to.x.toInt, to.y.toInt).toString + "[%.2f".format(len) + "/%.2f".format(dist) + "/%.2f]".format(diff)
 
   def getNodes = List(from, to)
 
@@ -22,13 +22,14 @@ class Edge(val from: GNode, val to: GNode) {
     MyLog.myPrintln(toString)
     val deltaX = to.x - from.x
     val deltaY = to.y - from.y
-    MyLog.myPrintln("                 ",deltaX,deltaY)
-    diff = len - Math.sqrt((deltaX * deltaX) + (deltaY * deltaY))
-    from.x = from.x - (getSign(diff) * getSign(deltaX))
-    from.y = from.y - (getSign(diff) * getSign(deltaY))
-    to.x = to.x + (getSign(diff) * getSign(deltaX))
-    to.y = to.y + (getSign(diff) * getSign(deltaY))
-    MyLog.myPrintln("                 ",toString)
+    dist = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY))
+    diff = len - dist
+    val inc = Math.abs(Math.log(Math.abs(diff)))
+    MyLog.myPrintln("                 ", deltaX, deltaY, inc)
+    from.x = from.x - (getSign(diff) * getSign(deltaX) * inc)
+    from.y = from.y - (getSign(diff) * getSign(deltaY) * inc)
+    to.x = to.x + (getSign(diff) * getSign(deltaX) * inc)
+    to.y = to.y + (getSign(diff) * getSign(deltaY) * inc)
   }
 
   def paint(g: Graphics2D): Unit = {
