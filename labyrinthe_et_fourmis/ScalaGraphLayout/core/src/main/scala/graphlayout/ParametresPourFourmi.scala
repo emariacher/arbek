94f24ebe7f962b02c06a1b0a968d3fdf940c59d3
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2019-06-16 10:00:58 +0200

    Agrégats lentement je bâtit un outil pour faire des stats sans stress

--- labyrinthe_et_fourmis/ScalaGraphLayout/core/src/main/scala/graphlayout/ParametresPourFourmi.scala
+++ labyrinthe_et_fourmis/ScalaGraphLayout/core/src/main/scala/graphlayout/ParametresPourFourmi.scala
@@ -1,12 +1,16 @@
 package graphlayout
 
+import java.util.zip.CRC32
+
 import kebra.MyLog._
 
 object ParametresPourFourmi {
+  var version = 1
+  var hash = hashCode()
+  var crcvar: Long = 0
   var nombreDefourmisParTribu = 4
   var limiteArrete = 120
   var stabilisationRassemble = 100
-
 
   var CEstLaFourmiliere = 20.0
   var influenceDesPheromones = 40.0
@@ -26,5 +30,18 @@ object ParametresPourFourmi {
       //f.setAccessible(true)
       myPrintDln("" + f.toString.split("\\.").last + " [" + f.get(this) + "]")
     })
+    myPrintln(getFields)
+    myPrintln(getValues)
+  }
+
+  def getFields = getClass.getDeclaredFields.toList.map(_.toString.split("\\.").last).mkString("", ", ", "")
+
+  def getValues = {
+    val crc = new CRC32
+    crcvar = 0
+    crc.update(getClass.getDeclaredFields.toList.map(_.get(this)).mkString("", ", ", "").getBytes)
+    crcvar = crc.getValue
+    val line = getClass.getDeclaredFields.toList.map(_.get(this)).mkString("", ", ", "")
+    line
   }
 }
