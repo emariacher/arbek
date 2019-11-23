b2feca79d8bd713a28b1328c7a0d7a772bc257f7
Eric Mariacher <eric.mariacher@gmail.com>
2019-11-23 08:47:26 +0100

    Euler518

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -33,79 +33,6 @@ class EulerMain extends FlatSpec with Matchers {
       z.flatten.sum
     }
 
-    def S2(n: Int, i: Int) = {
-      val t_iciS = timeStamp(t_start, "")
-      val p = EulerPrime.premiers10000.filter(_ < n).toList
-      val y = p.sliding(i).map(slide => slide.combinations(3).filter(Yes2(_)).toList).flatten.toList
-      val z = (ListSet.empty[List[BigInt]] ++ y).toList.sortBy(_.apply(1)).sortBy(_.head)
-      //println("S2 y(" + n + "," + i + ")", y.mkString("\n  ", "\n  ", "\n  "), y.map(_.sum), y.flatten.sum)
-      println("S2(" + n + "," + i + ")", p.length, z.length, z.mkString("\n  ", " - ", "\n  "), z.map(_.sum), z.map(_.sum).sum)
-      val t_laS = timeStamp(t_iciS, "la! S2(" + n + ")")
-      z.map(_.sum).sum
-    }
-
-    def T(n: Int) = {
-      val p = EulerPrime.premiers10000.filter(_ < n).toList
-      val y = p.combinations(3).filter(Yes2(_)).toList.flatten
-      val z = (ListSet.empty[BigInt] ++ y).toList.sorted
-      println("T(" + n + ")", p.length, z.length, "\n" + z, "\n" + p)
-      z
-    }
-
-    def S3(n: Int) = {
-      val t_iciS = timeStamp(t_start, "")
-      val prems = EulerPrime.premiers10000.filter(_ < n).toList
-      var pg = ListSet[BigInt](2)
-      var z: BigInt = 0
-      var l = ListSet.empty[List[BigInt]]
-      prems.foreach(p => {
-        if (pg.contains(p)) {
-          val x = prems.toList.combinations(2).toList.filter(c => Yes2(List(p, c.head, c.last))).map(c => {
-            List(p, c.head, c.last).sorted
-          })
-          l = l ++ x
-          val y = (ListSet.empty[BigInt] ++ x.flatten).toList.sorted
-          val u = x.map(c => c.sum)
-          pg = pg ++ y
-          //println("*1*", p, x, y, u, pg.toList.sorted, "\n  ", l)
-        } else {
-          val x = pg.toList.combinations(2).toList.filter(c => Yes2(List(p, c.head, c.last))).map(c => {
-            List(p, c.head, c.last).sorted
-          })
-          l = l ++ x
-          val y = (ListSet.empty[BigInt] ++ x.flatten).toList.sorted
-          val u = x.map(c => c.sum)
-          pg = pg ++ y
-          //println("*2*", p, x, y, u, pg.toList.sorted, "\n  ", l)
-          if (pg.contains(p)) {
-            val x = prems.toList.combinations(2).toList.filter(c => Yes2(List(p, c.head, c.last))).map(c => {
-              List(p, c.head, c.last).sorted
-            })
-            l = l ++ x
-            val y = (ListSet.empty[BigInt] ++ x.flatten).toList.sorted
-            val u = x.map(c => c.sum)
-            pg = pg ++ y
-            //println("*3*", p, x, y, u, pg.toList.sorted, "\n  ", l)
-          }
-        }
-      })
-      z = l.toList.flatten.sum
-      val t_laS = timeStamp(t_iciS, "la! S3(" + n + ")")
-      println(pg.toList.sorted)
-      println("S3(" + n + ")", prems.length, l.toList.length, l.toList.sortBy(_.apply(1)).sortBy(_.head).mkString("\n  ", " - ", "\n  "), l.toList.map(_.sum).sorted, l.toList.flatten.sum)
-      z
-    }
-
-    /*def S4(n:Int) = {
-      val t_iciS = timeStamp(t_start, "")
-      val prems = EulerPrime.premiers10000.filter(_ < n)
-      val z = prems.map(p => (p,p+1,prems.toList.map(p2 => (p2,(p2+1).toDouble/(p+1).toDouble)))).toList.sortBy(_._1)
-      println(z.mkString("\n  ", "\n  ", "\n  "))
-      //println("S4(" + n + ")", prems.length, z.length, z.mkString("\n  ", " - ", "\n  "), z.map(_.sum).sorted, z.flatten.sum)
-      val t_laS = timeStamp(t_iciS, "la! S4(" + n + ")")
-
-    }*/
-
     YesV(37, 151, 607) shouldEqual true
     YesV(71, 83, 97) shouldEqual true
     Yes(2, 5, 11) shouldEqual true
@@ -117,16 +44,6 @@ class EulerMain extends FlatSpec with Matchers {
     Yes2(List(5, 2, 11)) shouldEqual true
     println("********************************")
     S(100) shouldEqual 1035
-    /*S2(100, 19) should be === 1035
-    S(200) should be === S2(200, 43)
-    T(100)
-    T(200)
-    S(60) should be === S3(60)
-    S(50) should be === S3(50)
-    S(200) should be === S3(200)
-    S(1000) should be === S3(1000)*/
-    S3(200)
-    //S4(100)
 
     var result = 0
     println("Euler518[" + result + "]")
