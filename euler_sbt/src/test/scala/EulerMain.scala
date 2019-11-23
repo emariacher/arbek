ed5a299ba4654956e9398be4c4806f6b60176eef
Eric Mariacher <eric.mariacher@gmail.com>
2019-11-23 15:06:37 +0100

    Euler518

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -28,16 +28,30 @@ class EulerMain extends FlatSpec with Matchers {
 
     def S(n: Int) = {
       //val t_iciS = timeStamp(t_start, "")
-      val prems = EulerPrime.premiers10000.filter(_ < n).toList
+      val prems: List[BigInt] = EulerPrime.premiers10000.filter(_ < n).toList
       val z = prems.combinations(3).filter(Yes2(_)).toList
       val lp = z.flatten.distinct.sorted
-      val y = z.map(l => (l, div(l)))
+      val y = z.map(l => (l, div(l))).groupBy(_._1.head).toList.sortBy(_._1)
       println("S(" + n + ")", prems.length, z.length, z.flatten.sum,
         /*lp, "\n  ",
         prems.filter(p => !lp.contains(p)),*/
-        y.mkString("\n  ", " - ", "\n  "), z.map(_.sum).sorted)
+        y.mkString("\n   ", "\n   ", "\n   "), z.map(_.sum).sorted)
       //val t_laS = timeStamp(t_iciS, "la! S(" + n + ")")
       (z.flatten.sum, y)
+    }
+
+    def T(a: BigInt, prems: List[BigInt]): List[BigInt] = {
+      val t_iciS = timeStamp(t_start, "")
+      val a1 = a.toDouble + 1.0
+      val z = prems.filter(_ > a).filter(b => {
+        val b1 = b.toDouble + 1.0
+        val ratio = b1 / a1
+        val c: Double = (b1 * ratio) - 1.0
+        //println(a, b, c, (c % 1 <= 0.00001))
+        (c % 1 <= 0.1)
+      })
+      timeStamp(t_iciS, "la! T(" + a + ")")
+      z
     }
 
     YesV(37, 151, 607) shouldEqual true
@@ -51,14 +65,21 @@ class EulerMain extends FlatSpec with Matchers {
     Yes2(List(5, 2, 11)) shouldEqual true
     println("********************************")
     S(100)._1 shouldEqual 1035
-    println("********************************")
+    /*println("********************************")
     S(16)
     S(32)
     S(64)
     S(128)
     S(256)
     S(512)
-    S(1024)
+    S(1024)*/
+    println("********************************")
+    val prems: List[BigInt] = EulerPrime.premiers10000.toList
+    println(T(2, prems))
+    println(T(3, prems))
+    println(T(5, prems))
+    println(T(7, prems))
+    println(T(11, prems))
 
     var result = 0
     println("Euler518[" + result + "]")
