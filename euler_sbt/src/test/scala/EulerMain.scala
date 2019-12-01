1fa2d36406080118834995313c6190fc7e16737e
Eric Mariacher <eric.mariacher@gmail.com>
2019-12-01 12:33:55 +0100

    Euler621 une 1ère base

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -9,10 +9,10 @@ import scala.math.BigInt
 class EulerMain extends FlatSpec with Matchers {
   "Euler621" should "be OK" in {
     println("Euler621")
-    val t10 = Euler.triangular(4)
+    val tr = Euler.triangular(50)
 
     def G1(a: BigInt): BigInt = {
-      val t = t10.takeWhile(_ <= a)
+      val t = tr.takeWhile(_ <= a)
       val l1 = t.map(z => (z, 3 * z)).filter(_._2 == a).toList
       val l2 = t.combinations(2).map(y => List(y :+ y.head, y :+ y.last)).flatten.map(z => (z, z.sum)).filter(_._2 == a).toList
       val l3 = t.combinations(3).map(z => (z, z.sum)).filter(_._2 == a).toList
@@ -23,12 +23,12 @@ class EulerMain extends FlatSpec with Matchers {
       BigInt(l1.size + (l2.size * 3) + (l3.size * 6))
     }
 
-    println(t10)
+    println(tr)
 
     G1(7)
     G1(9) shouldEqual 7
     G1(1000) shouldEqual 78
-    //G1(1000000) shouldEqual 2106
+    G1(1000000) shouldEqual 2106
 
     var result = 0
     println("Euler621[" + result + "]")
