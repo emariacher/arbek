adf77872f87f31f0f5e0f2321888b95a91e1c7da
Eric Mariacher <eric.mariacher@gmail.com>
2019-12-02 21:42:49 +0100

    Euler621 ça patauge...

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -11,16 +11,15 @@ class EulerMain extends FlatSpec with Matchers {
     println("Euler621")
     val tr = Euler.triangular(50)
 
-    def G1(a: BigInt): (BigInt, BigInt, (BigInt, BigInt, BigInt)) = {
+    def G1(a: BigInt, verbose: Boolean = false): (BigInt, BigInt, (BigInt, BigInt, BigInt)) = {
       val t = tr.takeWhile(_ <= a)
       val l1 = t.map(z => (z, 3 * z)).filter(_._2 == a).toList
       val l2 = t.combinations(2).map(y => List(y :+ y.head, y :+ y.last)).flatten.map(z => (z, z.sum)).filter(_._2 == a).toList
       val l3 = t.combinations(3).map(z => (z, z.sum)).filter(_._2 == a).toList
       val result = BigInt(l1.size + (l2.size * 3) + (l3.size * 6))
-      /*println(l1)
-      println(l2)
-      println(l3)
-      println(a, result, " ", l1.size, l2.size, l3.size)*/
+      if (verbose) {
+        println(a, result, " ", (l1.size, l2.size, l3.size), l1, l2, l3)
+      }
       (a, result, (l1.size, l2.size, l3.size))
     }
 
@@ -31,9 +30,12 @@ class EulerMain extends FlatSpec with Matchers {
     G1(1000)._2 shouldEqual 78
     //G1(1000000)._4 shouldEqual 2106
 
-    val j = 100
-    val a = (1 until j).map(G1(_)).groupBy(_._3).toList.sortBy(b => (b._1._1 * j * j) + (b._1._2 * j) + b._1._3)
+    val j = 1000
+    val a = (1 to j).map(G1(_)).groupBy(_._3).toList.
+      sortBy(b => (b._1._1 * j * j) + (b._1._2 * j) + b._1._3).map(b => (b._1, b._2.map(_._1)))
     println(a.mkString("\n"))
+    println(tr)
+    (1 to j).map(G1(_, true))
 
     var result = 0
     println("Euler621[" + result + "]")
