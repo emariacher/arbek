bb3dc7d844ea7d13d0269846067b268ac1bfe1af
Eric Mariacher <eric.mariacher@gmail.com>
2019-12-02 20:38:24 +0100

    Euler621

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -11,24 +11,29 @@ class EulerMain extends FlatSpec with Matchers {
     println("Euler621")
     val tr = Euler.triangular(50)
 
-    def G1(a: BigInt): BigInt = {
+    def G1(a: BigInt): (BigInt, BigInt, (BigInt, BigInt, BigInt)) = {
       val t = tr.takeWhile(_ <= a)
       val l1 = t.map(z => (z, 3 * z)).filter(_._2 == a).toList
       val l2 = t.combinations(2).map(y => List(y :+ y.head, y :+ y.last)).flatten.map(z => (z, z.sum)).filter(_._2 == a).toList
       val l3 = t.combinations(3).map(z => (z, z.sum)).filter(_._2 == a).toList
-      println(l1)
+      val result = BigInt(l1.size + (l2.size * 3) + (l3.size * 6))
+      /*println(l1)
       println(l2)
       println(l3)
-      println(l1.size, l2.size, l3.size)
-      BigInt(l1.size + (l2.size * 3) + (l3.size * 6))
+      println(a, result, " ", l1.size, l2.size, l3.size)*/
+      (a, result, (l1.size, l2.size, l3.size))
     }
 
     println(tr)
 
     G1(7)
-    G1(9) shouldEqual 7
-    G1(1000) shouldEqual 78
-    G1(1000000) shouldEqual 2106
+    G1(9)._2 shouldEqual 7
+    G1(1000)._2 shouldEqual 78
+    //G1(1000000)._4 shouldEqual 2106
+
+    val j = 100
+    val a = (1 until j).map(G1(_)).groupBy(_._3).toList.sortBy(b => (b._1._1 * j * j) + (b._1._2 * j) + b._1._3)
+    println(a.mkString("\n"))
 
     var result = 0
     println("Euler621[" + result + "]")
