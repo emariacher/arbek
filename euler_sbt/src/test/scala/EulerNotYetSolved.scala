d59ac4e8813065d3e27c3ef2d04c49c2986999f9
Eric Mariacher <eric.mariacher@gmail.com>
2019-12-06 19:00:13 +0100

    Euler650

--- euler_sbt/src/test/scala/EulerNotYetSolved.scala
+++ euler_sbt/src/test/scala/EulerNotYetSolved.scala
@@ -1,5 +1,8 @@
+import java.util.Calendar
+
 import Euler._
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
+
 import scala.collection.immutable.TreeSet
 import scala.math.BigInt
 
@@ -448,5 +451,48 @@ class EulerNotYetSolved extends FlatSpec with Matchers {
     println("Euler518[" + result + "]")
     result shouldEqual 0
 
+  }
+
+  "Euler621" should "be OK" in {
+    println("Euler621")
+    val tr = Euler.triangular(500)
+
+    def G1(a: BigInt, verbose: Boolean = false): (BigInt, BigInt, (BigInt, BigInt, BigInt)) = {
+      var t_la = Calendar.getInstance()
+      val t = tr.takeWhile(_ <= a)
+      val l1 = t.filter(_ * 3 == a).toList
+      val l2 = t.combinations(2).map(y => List(y :+ y.head, y :+ y.last)).flatten.filter(_.sum == a).toList
+      val l3 = t.combinations(3).filter(_.sum == a).toList
+      val result = BigInt(l1.size + (l2.size * 3) + (l3.size * 6))
+      if (verbose) {
+        println(a, result, " ", (l1.size, l2.size, l3.size), l1, l2, l3)
+        t_la = timeStamp(t_la, "G1 fin "+ a)
+      }
+      (a, result, (l1.size, l2.size, l3.size))
+    }
+
+    println(tr)
+    println(tr.filter(_ % 10 == 0))
+
+    G1(7, true)
+    G1(9, true)._2 shouldEqual 7
+    G1(10, true)._2
+    G1(100, true)._2
+    G1(1000, true)._2 shouldEqual 78
+    G1(10000, true)._2 shouldEqual 252
+    G1(100000, true)._2
+    2106 shouldEqual 3 * 3 * 3 * 78
+    //G1(1000000)._4 shouldEqual 2106
+
+    val j = 100
+    val a = (1 to j).map(G1(_)).groupBy(_._3).toList.
+      sortBy(b => (b._1._1 * j * j) + (b._1._2 * j) + b._1._3).map(b => (b._1, b._2.map(_._1)))
+    println(a.mkString("\n"))
+    println(tr)
+    (1 to j).map(G1(_, true))
+
+    var result = 0
+    println("Euler621[" + result + "]")
+    result shouldEqual 0
   }
 }
