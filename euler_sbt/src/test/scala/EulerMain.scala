249b008163af10fe974cbfd9a325702aef7c0832
Eric Mariacher <eric.mariacher@gmail.com>
2019-12-08 10:51:12 +0100

    Euler650 modulo

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -10,6 +10,14 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler650" should "be OK" in {
     println("Euler650")
 
+    // explore how modulo addition & multiplication work
+    def m(bi: BigInt, mod: BigInt = BigInt(1000000007)): BigInt = bi % mod
+
+    val mod = 23
+    (m(27, mod) + m(37, mod)) shouldEqual m(27 + 37, mod)
+    m(m(27, mod) * m(37, mod), mod) shouldEqual m(27 * 37, mod)
+    EulerPrime.isPrime(1000000007) shouldBe true
+
     def binomial(n: BigInt, k: BigInt): BigInt = {
       factorielle(n) / ((factorielle(k) * factorielle(n - k)))
     }
@@ -56,6 +64,14 @@ class EulerMain extends FlatSpec with Matchers {
       r
     }
 
+    def D3(n: BigInt, bn: List[BigInt], verbose: Boolean = false): List[BigInt] = {
+      val r = ((new EulerDivisors(bn.map(o => new EulerDiv(o).primes).flatten.map(m(_))).divisors.map(m(_))) :+ BigInt(1)).sorted
+      if (verbose) {
+        println("D(" + n + "): " + r.sum, r)
+      }
+      r
+    }
+
     D(5, B(5)).sum shouldEqual 5467
 
     def S(n: BigInt): BigInt = {
@@ -67,18 +83,34 @@ class EulerMain extends FlatSpec with Matchers {
     S(5) shouldEqual 5736
     S(10) shouldEqual BigInt("141740594713218418")
 
-    j = 13
+    j = 15
+    var t_la = Calendar.getInstance()
     var r: BigInt = 1
     prevl = List(BigInt(1), BigInt(1))
     (2 to j).foreach(n => {
-      prevl = B2(n, prevl, true)
-      r += D(n, prevl, true).sum
+      prevl = B2(n, prevl, n < 11)
+      r += D(n, prevl, n < 11).sum
+      timeStamp(t_la, "D & B2 " + j + " : " + n)
       n match {
         case 5 => r shouldEqual 5736
         case 10 => r shouldEqual BigInt("141740594713218418")
         case _ =>
       }
     })
+    t_la = timeStamp(t_la, "D & B2 " + j)
+    r = 1
+    prevl = List(BigInt(1), BigInt(1))
+    (2 to j).foreach(n => {
+      prevl = B2(n, prevl, n < 11)
+      r += m(D3(n, prevl, n < 11).sum)
+      timeStamp(t_la, "D3 & B2 " + j + " : " + n)
+      n match {
+        case 5 => r shouldEqual 5736
+        case 10 => m(r) shouldEqual m(BigInt("141740594713218418"))
+        case _ =>
+      }
+    })
+    t_la = timeStamp(t_la, "D3 & B2 " + j)
 
     var result = 0
     println("Euler650[" + result + "]")
