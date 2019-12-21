2b6d90f23fc64960ef8861539ed2079741eca166
Eric Mariacher <eric.mariacher@gmail.com>
2019-12-21 09:06:01 +0100

    Euler601

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -11,11 +11,21 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler601" should "be OK" in {
     println("Euler601")
 
-    def streak(n: BigInt): Unit = {
-      stream_zero_a_linfini.takeWhile(i => (n + i) % (i + 1) == 0).toList.foreach(i => println("" + (n + i) + " " + i))
+    def streak(n: BigInt): Int = {
+      val z = stream_zero_a_linfini.takeWhile(i => (n + i) % (i + 1) == 0).toList
+      z.foreach(i => print(", " + (n + i) + ": " + i))
+      println(" - streak(" + n + "): " + z.length)
+      z.length
     }
 
-    streak(13)
+    def P(s: BigInt, N: BigInt): Int = {
+      (2 until N.toInt).filter(streak(_) == s).length
+    }
+
+    streak(13) shouldEqual 4
+    P(3, 14) shouldEqual 1
+    P(6,1000000) shouldEqual 14286
+
     var result = 0
     println("Euler601[" + result + "]")
     result shouldEqual 0
