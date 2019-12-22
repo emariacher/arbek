9f0fda4b6123353dc994378083d611ed30b377ed
Eric Mariacher <eric.mariacher@gmail.com>
2019-12-22 09:36:20 +0100

    Euler601

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -24,37 +24,45 @@ class EulerMain extends FlatSpec with Matchers {
       (2 until N.toInt).filter(streak(_) == s).length
     }
 
-    def P2(sn: Int, e: Int, prev: BigInt): BigInt = {
-      sn match {
-        case 2 => (prev * sn) + (e % sn)
-        case _ => BigInt(0)
-      }
-    }
-
-    def Q2(sn: Int, elimit: Int, verbose: Boolean = false) = {
-      var prev = BigInt(0)
-      var e = 0
-      if (verbose) {
-        println((1 until elimit).map(i => ("P(sn," + math.pow(sn, i).toInt + ")[" + i + "]", P(sn, math.pow(sn, i).toInt))).mkString(", "))
-      }
-      (1 until elimit).foreach(e => {
-        prev = P2(sn, e, prev)
-        val p = P(sn, math.pow(sn, e + 1).toInt)
-        if (verbose) {
-          print(" - ", sn, e, p, prev)
-        }
-        p shouldEqual prev
-      })
-      println("")
-
-    }
-
     streak(13, true) shouldEqual 4
     P(3, 14) shouldEqual 1
     P(6, 1000000) shouldEqual 14286
     println((2 until 100).map(streak(_)).max)
     streak(61, true)
     println((1 until 10).map(i => ("P(" + i + ",100)", P(i, 100))).mkString(", "))
+
+    def Q2(sn: Int, elimit: Int, verbose: Boolean = false) = {
+      var prev = BigInt(0)
+      if (verbose) {
+        println((1 until elimit).map(e => ("P(sn," + math.pow(sn, e).toInt + ")[" + e + "]", P(sn, math.pow(sn, e).toInt))).mkString(", "))
+      }
+      (1 until elimit).foreach(e => {
+        prev = P2(sn, e, prev)
+        val p = P(sn, math.pow(sn, e + 1).toInt)
+        if (verbose) {
+          print(" - ", sn, e, (e % sn), p, prev)
+        }
+        prev shouldEqual p
+      })
+      println("")
+    }
+
+    def P2(sn: Int, e: Int, prev: BigInt): BigInt = {
+      sn match {
+        case 2 => (prev * sn) + (e % sn)
+        case 3 => (prev * sn) + (e match {
+          case 0 => 1
+          case 1 => 1
+          case 2 => -1
+          case 3 => 1
+          case 4 => -1
+          case 5 => 1
+          case 6 => -1
+          case _ => 0
+        })
+        case _ => BigInt(999)
+      }
+    }
 
     Q2(2, 15, true)
     Q2(3, 13, true)
