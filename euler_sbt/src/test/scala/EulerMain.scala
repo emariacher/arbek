369c3372233f8245bd76693e6da3fc1bf01af5fa
Eric Mariacher <eric.mariacher@gmail.com>
2020-01-22 22:21:22 +0100

    Euler632 ça va pas l'faire

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -27,14 +27,14 @@ class EulerMain extends FlatSpec with Matchers {
     }
 
     def Ck2(bi: BigInt) = {
-      (1 to bi.toInt).map(z => squareprimes(z)).groupBy(u => u).toList.map(y => (y._1, y._2.length))
+      (1 to bi.toInt).map(z => squareprimes(z)).groupBy(u => u).toList.map(y => (y._1, y._2.length)).sortBy(_._1)
     }
 
     Ck(0, 10) shouldEqual 7
     Ck(1, 10) shouldEqual 3
     Ck(3, 100000) shouldEqual 297
 
-    val l = (1 to 6).map(e => {
+    val l = (1 to 7).map(e => {
       val y = BigInt(math.pow(10, e).toInt)
       (y, Ck2(y))
     })
@@ -42,6 +42,23 @@ class EulerMain extends FlatSpec with Matchers {
     l.foreach(e => {
       println(e._1 + e._2.mkString)
     })
+
+    l.sliding(2).foreach(ee => {
+      println("\n" + ee.last._1)
+      ee.head._2.zip(ee.last._2).foreach(ff => {
+        print(", " + ff._1._1 + " " + (ff._1._2, ff._2._2, (ff._1._2 * 10 - ff._2._2)))
+      })
+    })
+
+    val primes = EulerPrime.premiers1000
+
+    println()
+    var prod = BigInt(1)
+    primes.take(10).foreach(p => {
+      prod = prod * p * p
+      print(", " + (p, prod))
+    })
+    println()
 
     var result = 0
     println("Euler632[" + result + "]")
