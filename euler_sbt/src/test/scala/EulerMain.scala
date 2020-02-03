6bcda1aeeb7e5e58c85a3c6bd96988ca7f4c5f44
Eric Mariacher <eric.mariacher@gmail.com>
2020-02-03 20:54:18 +0100

    Euler23

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -20,7 +20,7 @@ class EulerMain extends FlatSpec with Matchers {
     sumdiv(12) shouldEqual 16
     sumdiv(28) shouldEqual 28
 
-    val limit = 10000
+    var limit = 2000
     var abundantNumbers = (1 to limit).filter(i => sumdiv(i) > i).toList
     println(abundantNumbers.take(50))
     println(abundantNumbers.filter(_ % 2 != 0))
@@ -41,7 +41,7 @@ class EulerMain extends FlatSpec with Matchers {
     }
 
     def doZeJob2(abundantNumbers: List[Int], limit: Int) = {
-      var t_ici = timeStamp(t_start, "ici!")
+      var t_ici = timeStamp(t_start, "ici!", false)
       var l = List[Int]()
       abundantNumbers.foreach(i => {
         //println(i, abundantNumbers.map(j => j + i))
@@ -59,8 +59,38 @@ class EulerMain extends FlatSpec with Matchers {
 
     l1 shouldEqual l2
 
+    val lpair = l2.sliding(2).map(z => z.last - z.head).toList.lastIndexWhere(_ > 2)
     println("check that all even numbers are quickly always sum of 2 abundants numbers: " +
-      l2.sliding(2).map(z => z.last - z.head).toList.lastIndexWhere(_ > 2), l2.length)
+      lpair, l2.sliding(2).toList.apply(lpair), l2.length)
+
+    val zlpairnosum = l2.sliding(2).toList.filter(z => z.last - z.head > 2)
+    println(zlpairnosum)
+    val lpairnosum = ((1 to 23).toList ++ List(26, 28, 34, 46)).filter(_ % 2 == 0)
+    println(lpairnosum)
+
+    limit = zeLimit
+    abundantNumbers = (1 to limit).filter(i => sumdiv(i) > i).toList
+
+    val oddabundants = abundantNumbers.filter(_ % 2 != 0)
+    println(oddabundants)
+
+    def doZeJob3(abundantNumbers: List[Int], oddabundantNumbers: List[Int], limit: Int) = {
+      var t_ici = timeStamp(t_start, "ici!", false)
+      var l = List[Int]()
+      oddabundantNumbers.foreach(i => {
+        //println(i, abundantNumbers.map(j => j + i))
+        l = (l ++ abundantNumbers.filter(j => j <= (limit - i)).map(j => j + i)).distinct.sorted
+      })
+      l = l.filter(_ <= limit)
+      println("  " + l)
+
+      t_ici = timeStamp(t_ici, "doZeJob3 la!")
+      l
+    }
+
+    val l3 = doZeJob3(abundantNumbers, oddabundants, limit)
+
+    (1 to limit).filter(i => i % 2 != 0).filter(i => !l3.contains(i)).toList
 
     var result = 0
     println("Euler23[" + result + "]")
