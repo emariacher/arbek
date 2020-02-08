7c42fa190e1121480f21d4edbf9e3c33629241d8
Eric Mariacher <eric.mariacher@gmail.com>
2020-02-08 18:40:35 +0100

    Euler700

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -14,6 +14,8 @@ class EulerMain extends FlatSpec with Matchers {
     val root = BigInt("1504170715041707")
     val mod = BigInt("4503599627370517")
     ((root * 3) % mod) shouldEqual BigInt("8912517754604")
+    ((root * 3) % mod) < root shouldEqual true
+    ((root + ((root * 3) % mod)) % mod) shouldEqual BigInt("1513083232796311")
 
     var result = 0
     println("Euler700[" + result + "]")
