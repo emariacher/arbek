80cdf89b3fe522a1dbfffb2d69b470731c18384a
Eric Mariacher <eric.mariacher@gmail.com>
2020-02-15 16:37:22 +0100

    Euler700

--- euler_sbt/src/main/scala/EulerDiv.scala
+++ euler_sbt/src/main/scala/EulerDiv.scala
@@ -1,26 +1,24 @@
 /**
  * Created by mariachere on 26.05.2015.
  */
+
 import scala.collection.immutable.TreeSet
 import scala.collection.immutable.NumericRange
 
-class EulerDiv(bi: BigInt) {
+class EulerDiv(bi: BigInt, verbose: Boolean = false) {
   var primes = List[BigInt]()
-  val premiers =  EulerPrime.premiers100000
+  val premiers = EulerPrime.premiers100000
   var bic = bi
   var index = 0
 
   premiers.takeWhile((premier: BigInt) => {
-    //println("*i1* bi: "+bi+" bic: "+bic+" premier: "+premier+" "+(bic%premier==0&bic>=premier)+" "+primes)
     while (bic % premier == 0 & bic >= premier) {
       primes = primes :+ premier
       bic = bic / premier
     }
-    //println("*i2* bi: "+bi+" bic: "+bic+" premier: "+premier+" "+(bic%premier==0&bic>=premier)+" "+primes)
     if (bic == 1) {
       false
     } else if ((premier * premier) > bic) {
-      //println("*i3* bi: "+bi+" bic: "+bic+" premier: "+premier+" "+(bic%premier==0&bic>=premier)+" "+primes)
       if (bic != bi) {
         primes = primes :+ bic
         require(primes.product == bi)
@@ -35,16 +33,22 @@ class EulerDiv(bi: BigInt) {
   if (primes.isEmpty) {
     require(bi < (premiers.last * premiers.last), bi.toString + "<" + premiers.last + "*" + premiers.last + "(" + (premiers.last * premiers.last) + ")")
   } else {
+    if (verbose) {
+      println(bi, primes, bic, math.sqrt(bic.toDouble))
+    }
     require(bic < (premiers.last * premiers.last), bic.toString + "<" + premiers.last + "*" + premiers.last + "(" + (premiers.last * premiers.last) + ")")
   }
 }
 
 class EulerDivisors(val l: List[BigInt]) {
   val range = new NumericRange(1, l.size, 1, true).toList
+
   def this(ed: EulerDiv) = this(ed.primes)
+
   val divisors = range.foldLeft(List[List[BigInt]]())(_ ++ l.combinations(_)).map(_.product).distinct.sorted
   val primesUnique = TreeSet[BigInt]() ++ l
-  def getFullDivisors = divisors ++ List[BigInt](1,l.product)
+
+  def getFullDivisors = divisors ++ List[BigInt](1, l.product)
 }
 
 class EulerDivisors3(l: List[BigInt]) {
@@ -59,6 +63,6 @@ class EulerDivisors2(val l: List[BigInt], val bi: BigInt) {
   var found = false
   result match {
     case Some(i) => found = true
-    case _       => found = false
+    case _ => found = false
   }
 }
--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -18,12 +18,14 @@ class EulerMain extends FlatSpec with Matchers {
     ((root + ((root * 3) % mod)) % mod) shouldEqual BigInt("1513083232796311")
 
     var eulercoinList = List((1, root, root.toString.length))
+    println(eulercoinList.map(_._2).sum, new EulerDiv(root, true).primes, eulercoinList)
     (1 to 1000000).foreach(n => {
       val bi = ((root * n) % mod)
       if (bi < eulercoinList.head._2) {
         eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
         val somme = eulercoinList.map(_._2).sum
         println(somme, eulercoinList)
+        println(somme, new EulerDiv(bi, true).primes, eulercoinList)
       }
     })
     println(root, EulerPrime.isPrime(root, true), root / 17, root % 17, BigInt("88480630296571") * 17)
@@ -31,6 +33,7 @@ class EulerMain extends FlatSpec with Matchers {
     println(BigInt("70841177179"), EulerPrime.isPrime(BigInt("70841177179"), true), BigInt("70841177179") / 12043)
     println(BigInt("5882353"), EulerPrime.isPrime(BigInt("5882353"), true))
     //println(EulerPrime.isPrime(mod, true))
+
 
     var result = 0
     println("Euler700[" + result + "]")
