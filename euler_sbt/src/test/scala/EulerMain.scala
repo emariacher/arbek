00b8c56a72c3bb17187c5b71b543dff837d27379
Eric Mariacher <eric.mariacher@gmail.com>
2020-02-15 16:23:08 +0100

    Euler700

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -18,18 +18,19 @@ class EulerMain extends FlatSpec with Matchers {
     ((root + ((root * 3) % mod)) % mod) shouldEqual BigInt("1513083232796311")
 
     var eulercoinList = List((1, root, root.toString.length))
-    (1 to 10000).foreach(n => {
+    (1 to 1000000).foreach(n => {
       val bi = ((root * n) % mod)
       if (bi < eulercoinList.head._2) {
         eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
-        println(eulercoinList)
+        val somme = eulercoinList.map(_._2).sum
+        println(somme, eulercoinList)
       }
     })
     println(root, EulerPrime.isPrime(root, true), root / 17, root % 17, BigInt("88480630296571") * 17)
     println(BigInt("88480630296571"), EulerPrime.isPrime(BigInt("88480630296571"), true), BigInt("88480630296571") / 1249)
     println(BigInt("70841177179"), EulerPrime.isPrime(BigInt("70841177179"), true), BigInt("70841177179") / 12043)
     println(BigInt("5882353"), EulerPrime.isPrime(BigInt("5882353"), true))
-    println(EulerPrime.isPrime(mod, true))
+    //println(EulerPrime.isPrime(mod, true))
 
     var result = 0
     println("Euler700[" + result + "]")
