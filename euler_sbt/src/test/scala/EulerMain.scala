9fce4651331e69fcc024467b5325799c3b6955f4
Eric Mariacher <eric.mariacher@gmail.com>
2020-02-16 11:21:18 +0100

    Euler700 peut-être que ça avance...

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -17,11 +17,16 @@ class EulerMain extends FlatSpec with Matchers {
     ((root * 3) % mod) < root shouldEqual true
     ((root + ((root * 3) % mod)) % mod) shouldEqual BigInt("1513083232796311")
 
-    var eulercoinList = List((1, root, root.toString.length))
     val biggestPrime = 220000
     val premiers = (new CheckEulerPrime(biggestPrime, 10000)).premiers
-    println(eulercoinList.map(_._2).sum, new EulerDiv2(root, premiers).primes)
-    var max = 43000000
+    val rootprimes = new EulerDiv2(root, premiers).primes
+    val rootdivisors = new EulerDivisors(rootprimes).getFullDivisors
+    val nearmod = rootdivisors.map(i => (i, mod / i))
+    println(root, rootprimes, rootdivisors)
+    println(nearmod, "\n")
+
+    var eulercoinList = List((1, root, root.toString.length))
+    var max = 60000000
     var t_la = Calendar.getInstance()
     (1 to max).foreach(n => {
       val bi = ((root * n) % mod)
@@ -29,21 +34,23 @@ class EulerMain extends FlatSpec with Matchers {
         eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
         val somme = eulercoinList.map(_._2).sum % mod
         //println(somme, bi, new EulerDiv2(bi, premiers).primes, eulercoinList)
-        println(somme, bi, eulercoinList.tail.head._2 - bi, n - eulercoinList.tail.head._1, eulercoinList)
+        //println(somme, bi, eulercoinList.tail.head._2 - bi, n - eulercoinList.tail.head._1, eulercoinList)
       }
     })
     var vieilleSomme = eulercoinList.map(_._2).sum % mod
     t_la = timeStamp(t_la, "après1 ")
     var n = 1
+    var prevdiff = 1
     eulercoinList = List((1, root, root.toString.length))
     while (n < max) {
       val bi = ((root * n) % mod)
       if (bi < eulercoinList.head._2) {
         eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
         val somme = eulercoinList.map(_._2).sum % mod
-        //println(somme, bi, new EulerDiv2(bi, premiers).primes, eulercoinList)
-        println(somme, bi, eulercoinList.tail.head._2 - bi, n - eulercoinList.tail.head._1, eulercoinList)
-        n += (n - eulercoinList.tail.head._1) - 1
+        println(somme, bi, eulercoinList.tail.head._2 - bi, n - eulercoinList.tail.head._1, (n - eulercoinList.tail.head._1).toDouble / prevdiff.toDouble, eulercoinList)
+        println(nearmod.map(z => (z._1, math.abs(((z._2 - bi)).toDouble))).sortBy(_._2).take(2))
+        prevdiff = n - eulercoinList.tail.head._1
+        n += prevdiff - 1
       } else {
         n += 1
       }
