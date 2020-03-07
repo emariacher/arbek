d59c23689417d501c0163d8a7d71f0041179dbb8
Eric Mariacher <eric.mariacher@gmail.com>
2020-03-07 13:56:51 +0100

    find min in list of list

--- bac_a_sable/python/list_min.py
+++ bac_a_sable/python/list_min.py
@@ -1,2 +1,10 @@
 thislist = [[6,3,4],[7,3,4],[6,0,4],[1,1,1]]
+print(thislist)
 print(min(thislist))
+print(list(map(min, thislist)))
+zemin = min(list(map(min, thislist)))
+print(zemin)
+lignemin = list(map(min, thislist)).index(zemin)
+print(lignemin)
+colonnemin = thislist[lignemin].index(zemin)
+print(colonnemin)
