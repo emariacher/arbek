9222615d55b8cd2d11b5178b3a5508aa9b440d9f
Eric Mariacher <eric.mariacher@gmail.com>
2020-03-29 10:20:41 +0200

    Euler700

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -50,23 +50,25 @@ class EulerMain extends FlatSpec with Matchers {
     var state = 0
     var n = 1
     var prevdiff = 1
+    var prevremdiff = 1.0
     var prevdelta: (BigInt, Double, Double) = null
     var prevy = 1.0
     var prevx = 1.0
     eulercoinList = List((1, root, root.toString.length))
     var max1 = 60000000
+    var lastEulerCoin = BigInt(0)
     while (n < max1) {
       val bi = ((root * n) % mod)
-      if (bi < eulercoinList.head._2) {
+      lastEulerCoin = eulercoinList.head._2
+      if (bi < lastEulerCoin) {
         eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
         val somme = eulercoinList.map(_._2).sum % mod
         val x = somme.toDouble / n.toDouble
         val diff = n - eulercoinList.tail.head._1
         val remdiff = math.abs((diff.toDouble * root.toDouble) / mod.toDouble) % 1.0
-        println("\n[" + n, somme, "" + bi + "] diff:", diff, remdiff,
+        println("\n[" + n, somme, "" + bi + " < " + lastEulerCoin + "] diff:", diff, remdiff + " vs " + prevremdiff,
           (n - eulercoinList.tail.head._1).toDouble / prevdiff.toDouble)
         val y = math.abs((diff.toDouble * root.toDouble) / mod.toDouble)
-
         val a = (-20 to 6600).map(u => (u, math.abs(((diff.toDouble + u) * root.toDouble) / mod.toDouble) % 1.0)).filter(u => u._2 > 0.999)
         val b = (0 to 200).toList.map(v => (0 to 200).toList.
           map(u => (u + (v * 500), math.abs(((diff.toDouble + u + (v * 500)) * root.toDouble) / mod.toDouble) % 1.0))).flatten.filter(u => u._2 > 0.999)
@@ -75,6 +77,7 @@ class EulerMain extends FlatSpec with Matchers {
         val z = nearmod.map(z => (z._1, 0.0, math.abs(((z._2 - bi)).toDouble))).sortBy(_._3)(Ordering.Double.TotalOrdering)
         prevdelta = z.head
         prevdiff = diff
+        prevremdiff = remdiff
         prevy = y
         prevx = x
         n += prevdiff
