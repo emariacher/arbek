6eea849a7a905a9f82eab3e626fa94ce03f37e95
Eric Mariacher <eric.mariacher@gmail.com>
2020-04-04 08:18:52 +0200

    Euler700 RAZ

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -41,110 +41,12 @@ class EulerMain extends FlatSpec with Matchers {
         eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
         val somme = eulercoinList.map(_._2).sum % mod
         //println(somme, bi, new EulerDiv2(bi, premiers).primes, eulercoinList)
-        //println(somme, bi, eulercoinList.tail.head._2 - bi, n - eulercoinList.tail.head._1, eulercoinList)
+        println(somme, bi, eulercoinList.tail.head._2 - bi, n - eulercoinList.tail.head._1, eulercoinList)
       }
     })
     var oldlast = eulercoinList.map(_._2).last
     var vieilleSomme = eulercoinList.map(_._2).sum % mod
-    t_la = timeStamp(t_la, "après1 max: " + max)
-    var state = 0
-    var n = 1
-    var prevdiff = 1
-    var prevremdiff = 1.0
-    var prevdelta: (BigInt, Double, Double) = null
-    var prevy = 1.0
-    var prevx = 1.0
-    eulercoinList = List((1, root, root.toString.length))
-    var max1 = max + 4
-    var lastEulerCoin = BigInt(0)
-    while (n < max1) {
-      val bi = ((root * n) % mod)
-      lastEulerCoin = eulercoinList.head._2
-      if (bi < lastEulerCoin) {
-        eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
-        val somme = eulercoinList.map(_._2).sum % mod
-        val x = somme.toDouble / n.toDouble
-        val diff = n - eulercoinList.tail.head._1
-        val remdiff = math.abs((diff.toDouble * root.toDouble) / mod.toDouble) % 1.0
-        println("\n[" + n, somme, "" + bi + " < " + lastEulerCoin + "] diff:", diff, "" + remdiff + " vs " + prevremdiff,
-          (n - eulercoinList.tail.head._1).toDouble / prevdiff.toDouble)
-        val y = math.abs((diff.toDouble * root.toDouble) / mod.toDouble)
-        val a = (-20 to 6600).map(u => (u, math.abs(((diff.toDouble + u) * root.toDouble) / mod.toDouble) % 1.0)).filter(u => u._2 > 0.999)
-        val b = (0 to 200).toList.map(v => (0 to 200).toList.
-          map(u => (u + (v * 500), math.abs(((diff.toDouble + u + (v * 500)) * root.toDouble) / mod.toDouble) % 1.0))).flatten.filter(u => u._2 > 0.999)
-        println(y, "   ", a)
-        println(y, "   ", b)
-        val z = nearmod.map(z => (z._1, 0.0, math.abs(((z._2 - bi)).toDouble))).sortBy(_._3)(Ordering.Double.TotalOrdering)
-        prevdelta = z.head
-        prevdiff = diff
-        prevremdiff = remdiff
-        prevy = y
-        prevx = x
-        n += prevdiff
-        if ((n > max) & (eulercoinList.map(_._2).last == oldlast)) {
-          println("************Checking! @" + max + "**************")
-          eulercoinList.map(_._2).sum % mod shouldEqual vieilleSomme
-          timeStamp(t_la, "après2 max: " + max)
-        }
-        state = 1
-      } else if (state == 1) {
-        state = 2
-        n += prevdiff * 2
-      } else {
-        state = 2
-        n += 1
-      }
-    }
-    t_la = timeStamp(t_la, "après2 max1: " + max1)
-
-    state = 0
-    n = 1
-    prevdiff = 1
-    prevremdiff = 1.0
-    prevdelta = null
-    prevy = 1.0
-    prevx = 1.0
-    eulercoinList = List((1, root, root.toString.length))
-    var max2 = max + 4
-    lastEulerCoin = BigInt(0)
-    while (n < max2) {
-      val bi = ((root * n) % mod)
-      lastEulerCoin = eulercoinList.head._2
-      if (bi < lastEulerCoin) {
-        eulercoinList = (eulercoinList :+ (n, bi, bi.toString.length)).sortBy(_._2)
-        val somme = eulercoinList.map(_._2).sum % mod
-        val x = somme.toDouble / n.toDouble
-        val diff = n - eulercoinList.tail.head._1
-        val remdiff = math.abs((diff.toDouble * root.toDouble) / mod.toDouble) % 1.0
-        println("\n[" + n, somme, "" + bi + " < " + lastEulerCoin + "] diff:", diff, "" + remdiff + " vs " + prevremdiff,
-          (n - eulercoinList.tail.head._1).toDouble / prevdiff.toDouble)
-        val y = math.abs((diff.toDouble * root.toDouble) / mod.toDouble)
-        val b = (0 to 500).toList.map(v => (1 to 500).toList.
-          map(u => (u + (v * 500), math.abs(((diff.toDouble + u + (v * 500)) * root.toDouble) / mod.toDouble) % 1.0))).flatten.filter(u => u._2 >= prevremdiff).take(4)
-        println(y, "   ", b)
-        val z = nearmod.map(z => (z._1, 0.0, math.abs(((z._2 - bi)).toDouble))).sortBy(_._3)(Ordering.Double.TotalOrdering)
-        prevdelta = z.head
-        prevdiff = diff
-        prevremdiff = remdiff
-        prevy = y
-        prevx = x
-        n += prevdiff
-        if ((n > max) & (eulercoinList.map(_._2).last == oldlast)) {
-          println("************Checking! @" + max + "**************")
-          eulercoinList.map(_._2).sum % mod shouldEqual vieilleSomme
-          timeStamp(t_la, "après2 max: " + max)
-        }
-        state = 1
-      } else if (state == 1) {
-        state = 2
-        n += prevdiff * 2
-      } else {
-        state = 2
-        n += 1
-      }
-    }
-    t_la = timeStamp(t_la, "après2 max1: " + max1)
-
+    t_la = timeStamp(t_la, "après1 max: " + max + " " + vieilleSomme)
 
     var result = 0
     println("Euler700[" + result + "]")
