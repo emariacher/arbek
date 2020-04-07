46f506c68b001a0db1e466e968e4204d0e2590df
Eric Mariacher <eric.mariacher@gmail.com>
2020-04-07 20:56:41 +0200

    Euler700

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -64,7 +64,7 @@ class EulerMain extends FlatSpec with Matchers {
         val diff = n - eulercoinList.tail.head._1
         println("\n" + n, somme, bi, eulercoinList.tail.head._2 - bi, n - eulercoinList.tail.head._1, (n - eulercoinList.tail.head._1).toDouble / prevdiff.toDouble, eulercoinList)
         println("[" + n, somme, "" + bi + "]", n * bi, somme / n, x / prevx, eulercoinList.tail.head._2 - bi,
-          " prevdiff[" + diff + "]", (n - eulercoinList.tail.head._1).toDouble / prevdiff.toDouble)
+          " diff[" + diff + " / " + ((diff.toDouble * root.toDouble) / mod.toDouble) + "]", diff.toDouble / prevdiff.toDouble)
         val y = math.abs((diff.toDouble * root.toDouble) / mod.toDouble)
         //println(y, "   ", (-4 to 4).map(u => math.abs(((diff.toDouble + u) * root.toDouble) / mod.toDouble)))
         val z = nearmod.map(z => (z._1, 0.0, math.abs(((z._2 - bi)).toDouble))).sortBy(_._3)(Ordering.Double.TotalOrdering)
