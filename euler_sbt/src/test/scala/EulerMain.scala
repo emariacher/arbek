f4e845b072994a0af5011683f2c81498587285cf
Eric Mariacher <eric.mariacher@gmail.com>
2020-04-13 09:28:18 +0200

    Euler706

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -10,11 +10,13 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler706" should "be OK" in {
     println("Euler706")
 
-    def f(n: BigInt): BigInt = {
+    def f(n: BigInt, verbose: Boolean = false): BigInt = {
       val s = n.toString
       val l = s.length
       val z = (1 to l).map(i => s.toSeq.sliding(i)).toList.flatten
-      //println(n, z.filter(u => (u.sum % 3 == 0)).length, z)
+      if(verbose) {
+        println(n, z.filter(u => (u.sum % 3 == 0)).length, z)
+      }
       z.filter(u => (u.sum % 3 == 0)).length
     }
 
@@ -26,29 +28,34 @@ class EulerMain extends FlatSpec with Matchers {
       z
     }
 
-    f(2573) shouldEqual 3
-    val z = (2 to 6).map(i => (i, F(i)))
+    f(2573,true) shouldEqual 3
+    f(100,true) shouldEqual 3
+    f(150,true) shouldEqual 3
+    f(1000,true) shouldEqual 6
+    /*val z = (2 to 6).map(i => (i, F(i)))
     z.apply(0) shouldEqual(2, 30)
-    z.apply(4) shouldEqual(6, 290898)
+    z.apply(4) shouldEqual(6, 290898)*/
 
-    def f2(n: BigInt): BigInt = {
+    def f2(n: BigInt): List[BigInt] = {
       val s = n.toString
       val l = s.length
       val z = (1 to l).map(i => s.toSeq.sliding(i)).toList.flatten
-      println(n, z.filter(u => (u.sum % 3 == 0)).length, z)
-      z.filter(u => (u.sum % 3 == 0)).length
+      //println(n, z.filter(u => (u.sum % 3 == 0)).length, z)
+      z.filter(u => (u.sum % 3 == 0)).map(u => BigInt(u.toString))
     }
 
     def F2(d: Int): BigInt = {
       val start = BigInt("1" + (2 to d).map(z => "0").mkString)
       val end = start * 10
-      val y = (start until end).toList.filter(bi => f2(bi) % 3 == 0)
+      val y = (start until end).toList.filter(bi => f2(bi).length % 3 == 0)
       val z = y.length
-      println(d, (start until end), z)
+      println(d, (start until end), z, y)
       z
     }
 
-    F2(2)
+    F2(2) shouldEqual 30
+    F2(3)
+    F2(4)
 
     var result = 0
     println("Euler706[" + result + "]")
