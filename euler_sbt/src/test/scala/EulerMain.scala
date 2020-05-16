0d8ec57de97fcdbbc4dcde4b8b7212e72d7f0576
Eric Mariacher <eric.mariacher@gmail.com>
2020-05-16 11:59:55 +0200

    Euler457

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -10,7 +10,15 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler457" should "be OK" in {
     println("Euler457")
 
+    def f(n: BigInt): BigInt = (n * n) - (3 * n) - 1
+
+    def OK(p: BigInt, n: BigInt): Boolean = f(n) % (p * p) == 0
+
     var t_la = Calendar.getInstance()
+    val premiers1000 = EulerPrime.premiers1000
+    premiers1000.foreach(p => {
+      println(p, p * p, (0 to 100).map(n => (n, f(n), OK(p, n))).filter(_._3).mkString("#"))
+    })
     t_la = timeStamp(t_la, "ici")
 
     var result = 0
