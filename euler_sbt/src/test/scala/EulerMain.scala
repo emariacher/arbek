ecf2dfd957709500713a6090b16c63d0aa6ed281
Eric Mariacher <eric.mariacher@gmail.com>
2020-05-18 18:48:50 +0200

    Euler457

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -16,9 +16,9 @@ class EulerMain extends FlatSpec with Matchers {
 
     var t_la = Calendar.getInstance()
     val premiers1000 = EulerPrime.premiers1000
-    premiers1000.foreach(p => {
-      println(p, p * p, (0 to 1000).map(n => (n, f(n), OK(p, n))).filter(_._3).mkString("#"))
-    })
+    println(premiers1000.toList.map(p => {
+      (p, p * p, (0 to 1000).map(n => (n, f(n), OK(p, n))).filter(_._3))
+    }).filter(!_._3.isEmpty).mkString("\n"))
     t_la = timeStamp(t_la, "ici")
     println((0 to 1000).map(n => (n, f(n))).map(nn => {
       (nn._1, nn._2, premiers1000.filter(p => nn._2 % (p * p) == 0).mkString("-"))
