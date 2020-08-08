a701ac2478405480e478a37e84d3e43fbf7a6b8b
Eric Mariacher <eric.mariacher@gmail.com>
2020-08-08 07:56:10 +0200

    Euler243 deadend! :-1:

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -10,7 +10,7 @@ class EulerMain extends FlatSpec with Matchers {
 
   "Euler243" should "be OK" in {
     println("Euler243")
-    val premiers = (new CheckEulerPrime(1540000, 10000)).premiers
+    val premiers = (new CheckEulerPrime(2000000, 10000)).premiers
 
     def resilience(d: Int) = {
       var t_ici = Calendar.getInstance()
@@ -35,18 +35,18 @@ class EulerMain extends FlatSpec with Matchers {
       val d = l.product
       val ld = l.distinct
       val lp1 = List(BigInt(1)) ++ premiers.takeWhile(_ < d).filter(bi => !ld.contains(bi))
-      val lp2 = lp1.takeWhile(p => (p * p) <= d).map(p => {
+      val lp2 = lp1.takeWhile(p => (p * p) <= d).flatMap(p => {
         //println("in lp2", p, lp1.takeWhile(bi => bi.toDouble < d.toDouble / p.toDouble).map(_ * p))
         lp1.takeWhile(bi => bi.toDouble < d.toDouble / p.toDouble).map(_ * p)
-      }).flatten.sorted.distinct
-      val lp3 = lp2.takeWhile(p => (p * p * p) <= d).map(p => {
+      }).sorted.distinct
+      val lp3 = lp2.takeWhile(p => (p * p * p) <= d).flatMap(p => {
         //println("in lp3", p, lp2.takeWhile(bi => bi.toDouble < d.toDouble / p.toDouble).map(_ * p))
         lp2.takeWhile(_.toDouble < d.toDouble / p.toDouble).map(_ * p)
-      }).flatten.sorted.distinct
-      val lp4 = lp3.takeWhile(p => (p * p * p * p) <= d).map(p => {
+      }).sorted.distinct
+      val lp4 = lp3.takeWhile(p => (p * p * p * p) <= d).flatMap(p => {
         //println("in lp4", p, lp3.takeWhile(bi => bi.toDouble < d.toDouble / p.toDouble).map(_ * p))
         lp3.takeWhile(_.toDouble < d.toDouble / p.toDouble).map(_ * p)
-      }).flatten.sorted.distinct
+      }).sorted.distinct
       /*println("  lp1", lp1)
       println("  lp2", lp2)
       println("  lp3", lp3)
@@ -54,7 +54,7 @@ class EulerMain extends FlatSpec with Matchers {
       val ls = lp4
       println("" + d + "\t" + ls.size + "/" + (d - 1) + "\t" + 1.0 * ls.size / (d.toDouble - 1),
         "ici3 " + timeStampD(t_ici, "ici", false))
-      if (!lp4.diff(lp3).isEmpty) {
+      if (lp4.diff(lp3).nonEmpty) {
         val pp = lp2.tail.head
         println(pp, math.pow(pp.toDouble, 4), d, "lp4.diff(lp3)", lp4.diff(lp3).map(bi => (bi, new EulerDiv(bi).primes)).take(2))
         (math.pow(pp.toDouble, 4) < d.toDouble) shouldEqual true
@@ -67,6 +67,7 @@ class EulerMain extends FlatSpec with Matchers {
     def resilience4(l: List[BigInt]) = {
       var t_ici = Calendar.getInstance()
       val d = l.product
+      print("" + d, l.last, "")
       val ld = l.distinct
       var lp = List(BigInt(1)) ++ premiers.takeWhile(_ < d).filter(bi => !ld.contains(bi))
       var oldlp = List[BigInt]()
@@ -81,7 +82,7 @@ class EulerMain extends FlatSpec with Matchers {
         exponent += 1
       }
       val ls = lp
-      println("" + d + "\t" + ls.size + "/" + (d - 1) + "\t" + 1.0 * ls.size / (d.toDouble - 1), exponent,
+      println("\t" + ls.size + "/" + (d - 1) + "\t" + 1.0 * ls.size / (d.toDouble - 1), exponent,
         "ici4 " + timeStampD(t_ici, "ici", false))
       (d, "" + ls.size + "/" + (d - 1), 1.0 * ls.size / (d.toDouble - 1), ls.sorted)
     }
@@ -89,7 +90,6 @@ class EulerMain extends FlatSpec with Matchers {
     var t_la = Calendar.getInstance()
     //resilience(12) shouldEqual(4, 11)
     println(1.0 * 15499 / 94744)
-    println(94745, new EulerDiv(94745).primes)
 
     resilience(2 * premiers.take(5).product.toInt)
     resilience(2 * 2 * 3 * 3 * premiers.take(5).product.toInt)
@@ -97,17 +97,15 @@ class EulerMain extends FlatSpec with Matchers {
       val z = resilience(premiers.take(i).product.toInt)
       val y = resilience4(premiers.take(i).toList)
       val ldiff = z._4.diff(y._4)
-      println(premiers.take(i).product.toInt, "ldiff", ldiff.size, ldiff.map(bi => (bi, new EulerDiv(bi).primes)).take(40))
-      println(y._1, y._2, y._3)
+      //println(premiers.take(i).product.toInt, "ldiff", ldiff.size, ldiff.map(bi => (bi, new EulerDiv(bi).primes)).take(40))
       z._4.diff(y._4).size shouldEqual 0
       val z3 = resilience(premiers.take(i).product.toInt * 3)
       val y3 = resilience4(premiers.take(i).toList :+ 3)
       val ldiff3 = z3._4.diff(y3._4)
-      println(premiers.take(i).product.toInt * 3, "ldiff3", ldiff3.size, ldiff3.map(bi => (bi, new EulerDiv(bi).primes)).take(40))
-      println(y3._1, y3._2, y3._3)
+      //println(premiers.take(i).product.toInt * 3, "ldiff3", ldiff3.size, ldiff3.map(bi => (bi, new EulerDiv(bi).primes)).take(40))
       z3._4.diff(y3._4).size shouldEqual 0
     })
-    t_la = timeStamp(t_la, "là")
+    t_la = timeStamp(t_la, "là 1")
 
     val result = 0
     println("Euler243[" + result + "]")
