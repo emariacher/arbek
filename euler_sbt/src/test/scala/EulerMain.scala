9735a6d74f3ddb51ccf902be5130786fcd7348d7
Eric Mariacher <eric.mariacher@gmail.com>
2020-08-09 16:50:36 +0200

    Euler493

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -10,6 +10,16 @@ class EulerMain extends FlatSpec with Matchers {
 
   "Euler493" should "be OK" in {
     println("Euler493")
+
+    def dozejob(s: String, c: Int) = {
+      val z = s.toSeq.combinations(c).toList
+      z.map(e => e.toSeq.distinct.size)
+      println(s, z.map(e => e.toSeq.distinct.size).sum.toDouble / z.size.toDouble, z.size, z)
+    }
+
+    dozejob("11223344", 4)
+    dozejob("111222333444555", 4)
+
     val result = 0
     println("Euler493[" + result + "]")
     result shouldEqual 0
