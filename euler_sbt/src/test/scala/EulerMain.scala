28f3f890e2c19543649dda07168ad7af971e0249
Eric Mariacher <eric.mariacher@gmail.com>
2020-08-10 17:53:33 +0200

    Euler493

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -11,19 +11,32 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler493" should "be OK" in {
     println("Euler493")
 
-    def dozejob1(s: String, c: Int) = {
-      val z = s.toSeq.combinations(c).toList
+    def dozejob1(s: String, sampleSize: Int) = {
+      val z = s.toSeq.combinations(sampleSize).toList
       val y = z.map(e => e.toSeq.distinct.size).groupBy(u => u)
       val x = y.toList.map(a => (a._1, a._2.size)).sortBy(_._1)
       val w = x.map(a => a._1 * a._2).sum
       w shouldEqual z.map(e => e.toSeq.distinct.size).sum
-      println(c, s, w.toDouble / z.size.toDouble, z.size, z.take(5))
+      println(sampleSize, w.toDouble / z.size.toDouble, z.size, z.take(5))
       println("  " + x)
       x.map(_._2).sum shouldEqual z.size
     }
 
-    var s = "1111122222333334444455555"
+    def builds(couleur: Int, parCouleur: Int): String = {
+      (1 to couleur).map(c => {
+        (1 to parCouleur).map(u => c.toString).mkString("")
+      }).mkString("")
+    }
+
+    var s = builds(5, 6)
+    println(s)
     (1 to 12).foreach(i => {
+      dozejob1(s, i)
+    })
+
+    s = builds(5, 7)
+    println(s)
+    (1 to 14).foreach(i => {
       dozejob1(s, i)
     })
 
