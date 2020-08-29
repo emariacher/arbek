be0a33397b5c18cbf8c4e0b8a5ad1b90b4f1257a
Eric Mariacher <eric.mariacher@gmail.com>
2020-08-29 08:17:36 +0200

    Euler493

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -19,6 +19,10 @@ class EulerMain extends FlatSpec with Matchers {
       w shouldEqual z.map(e => e.toSeq.distinct.size).sum
       println(sampleSize, w.toDouble / z.size.toDouble, z.size, z.take(5))
       println("  " + x)
+      if (prev.nonEmpty) {
+        val v = prev.dropWhile(c => c._1 < x.head._1)
+        println("    " + x.zip(v).map(c => (c._1._1, c._1._2 - c._2._2)))
+      }
       x.map(_._2).sum shouldEqual z.size
       x
     }
