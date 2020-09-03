dd501dbe19b6b66687f0e90251b47e7d936f351e
Eric Mariacher <eric.mariacher@gmail.com>
2020-09-03 21:34:17 +0200

    Euler33

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -8,63 +8,23 @@ import scala.math.BigInt
 class EulerMain extends FlatSpec with Matchers {
 
 
-  "Euler493" should "be OK" in {
-    println("Euler493")
+  "Euler33" should "be OK" in {
+    println("Euler33")
 
-    def dozejob1(s: String, sampleSize: Int, prev: List[(Int, Int)]) = {
-      val z = s.toSeq.combinations(sampleSize).toList
-      val y = z.map(e => e.toSeq.distinct.size).groupBy(u => u)
-      val x = y.toList.map(a => (a._1, a._2.size)).sortBy(_._1)
-      val w = x.map(a => a._1 * a._2).sum
-      w shouldEqual z.map(e => e.toSeq.distinct.size).sum
-      println(sampleSize, w.toDouble / z.size.toDouble, z.size, z.take(5), z.last)
-      println("  " + x)
-      if (prev.nonEmpty) {
-        val v = prev.dropWhile(c => c._1 < x.head._1)
-        println("    " + x.zip(v).map(c => (c._1._1, c._1._2 - c._2._2)))
-      }
-      x.map(_._2).sum shouldEqual z.size
-      x
-    }
+    val premiers100 = EulerPrime.premiers1000.filter(_ < 100)
+    val paspremiers100 = (10 to 99).filter(i => !premiers100.contains(i))
 
-    def builds(couleur: Int, parCouleur: Int): String = {
-      (1 to couleur).map(c => {
-        (1 to parCouleur).map(u => c.toString).mkString("")
-      }).mkString("")
-    }
+    println(paspremiers100)
 
-    def list2string(l: List[Int]) = {
-      l.zipWithIndex.map(i => (1 to i._1).toList.map(u => i._2).mkString("")).mkString("")
-    }
-
-    def dozejob2(s: String, sampleSize: Int, prev: List[(Int, Int)]) = {
-      val z = s.toSeq.combinations(sampleSize).toList
-      val y = z.map(e => e.toSeq.distinct.size).groupBy(u => u)
-      val x = z.map(e => e.groupBy(f => f).toList.map(g => g._2.size).sorted)
-      println(sampleSize, z.map(e => e.toSeq.distinct.size).sum.toDouble / z.size.toDouble, z.size)
-      //println("  " + z.zip(x).groupBy(_._2.toString))
-      println("  " + z.zip(x).groupBy(_._2.toString).toList.map(i => (i._1, list2string(i._2.head._2), list2string(i._2.head._2).toSeq.permutations.length, i._2.length)).sortBy(_._1.toString))
-      if (prev.nonEmpty) {
-      }
-      x
-    }
-
-
-    var s = builds(4, 5)
-    println(s)
-    (1 to 10).foreach(i => {
-      dozejob2(s, i, List[(Int, Int)]())
+    val z = paspremiers100.map(i => {
+      paspremiers100.dropWhile(_ < i).filter(j => i.toString.toList.intersect(j.toString.toList).nonEmpty).map(j => {
+        (i, j, i.toDouble / j.toDouble)
+      })
     })
-
-    /*s = builds(7, 10)
-    println(s)
-    var prev = List[(Int, Int)]()
-    (1 to 20).foreach(i => {
-      prev = dozejob1(s, i, prev)
-    })*/
+    println(z.flatten.groupBy(_._3).filter(g => g._2.length > 1).toList.sortBy(_._1)(Ordering.Double.TotalOrdering).mkString("\n"))
 
     val result = 0
-    println("Euler493[" + result + "]")
+    println("Euler33[" + result + "]")
     result shouldEqual 0
   }
 
