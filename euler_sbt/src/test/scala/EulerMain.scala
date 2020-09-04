dbf46250280cb279bd8d33b6c7ff360083f836f5
Eric Mariacher <eric.mariacher@gmail.com>
2020-09-04 20:18:31 +0200

    Euler33

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -12,16 +12,16 @@ class EulerMain extends FlatSpec with Matchers {
     println("Euler33")
 
     val premiers100 = EulerPrime.premiers1000.filter(_ < 100)
-    val paspremiers100 = (10 to 99).filter(i => !premiers100.contains(i))
+    val paspremiers100 = (10 to 99).filter(i => !premiers100.contains(i)).filter(_.toString.indexOf("0") < 0)
 
     println(paspremiers100)
 
     val z = paspremiers100.map(i => {
-      paspremiers100.dropWhile(_ < i).filter(j => i.toString.toList.intersect(j.toString.toList).nonEmpty).map(j => {
-        (i, j, i.toDouble / j.toDouble)
+      paspremiers100.dropWhile(_ <= i).filter(j => i.toString.toList.intersect(j.toString.toList).nonEmpty).map(j => {
+        (i, j, i.toString.toList.intersect(j.toString.toList).head, i.toDouble / j.toDouble)
       })
     })
-    println(z.flatten.groupBy(_._3).filter(g => g._2.length > 1).toList.sortBy(_._1)(Ordering.Double.TotalOrdering).mkString("\n"))
+    println(z.flatten.groupBy(_._4).filter(g => g._2.length > 1).toList.sortBy(_._1)(Ordering.Double.TotalOrdering).mkString("\n"))
 
     val result = 0
     println("Euler33[" + result + "]")
