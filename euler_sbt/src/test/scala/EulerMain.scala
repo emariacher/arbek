25802e87c5849d80e5247417824c6ea45308af60
Eric Mariacher <eric.mariacher@gmail.com>
2020-09-05 14:08:24 +0200

    Euler33 j'ai trouvé les fractions mais le résultat ne colle pas

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -11,31 +11,25 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler33" should "be OK" in {
     println("Euler33")
 
-    val premiers100 = EulerPrime.premiers1000.filter(_ < 100)
-    val paspremiers100 = (10 to 99).filter(i => !premiers100.contains(i)).filter(_.toString.indexOf("0") < 0)
+    val p100 = (10 to 99).filter(_.toString.indexOf("0") < 0)
 
-    println(paspremiers100)
-
-    val z = paspremiers100.map(i => {
-      paspremiers100.dropWhile(_ <= i).filter(j => i.toString.toList.intersect(j.toString.toList).nonEmpty).map(j => {
+    val z = p100.map(i => {
+      p100.dropWhile(_ <= i).filter(j => i.toString.toList.intersect(j.toString.toList).nonEmpty).map(j => {
         val k = i.toString.toList.intersect(j.toString.toList).head
-        if (i.toString.toList.exists(_ != k) && j.toString.toList.exists(_ != k)) {
-          val is = i.toString.toList.filter(_ != k).head.toString
-          val js = j.toString.toList.filter(_ != k).head.toString
-          if ((i.toDouble / j.toDouble == is.toDouble / js.toDouble) || (i.toDouble / j.toDouble == js.toDouble / is.toDouble)) {
-            println("************", i, j, k, i.toDouble / j.toDouble, is, js, is.toDouble / js.toDouble, js.toDouble / is.toDouble)
-          }
-          (i, j, k, i.toDouble / j.toDouble, is, js, is.toDouble / js.toDouble, js.toDouble / is.toDouble)
-        } else {
-          (i, j, k, i.toDouble / j.toDouble, 0, 0, 0, 0)
-        }
+        (i, j, k)
       })
-    })
-    println(z.flatten.groupBy(_._4).filter(g => g._2.length > 1).toList.sortBy(_._1)(Ordering.Double.TotalOrdering).mkString("\n"))
+    }).flatten.filter(ijk => {
+      ijk._1.toString.toList.exists(_ != ijk._3) && ijk._2.toString.toList.exists(_ != ijk._3)
+    }).map(ijk => {
+      val is = ijk._1.toString.toList.filter(_ != ijk._3).head.toString
+      val js = ijk._2.toString.toList.filter(_ != ijk._3).head.toString
+      (ijk._1, ijk._2, ijk._3, ijk._1.toDouble / ijk._2.toDouble, is, js, is.toDouble / js.toDouble)
+    }).filter(u => u._4 == u._7)
+    println(z.mkString("\n"))
 
-    val result = 0
+    val result = z.map(_._2.toInt).product
     println("Euler33[" + result + "]")
-    result shouldEqual 0
+    result shouldEqual 800
   }
 
 }
