7505b43422a41cc852fca8c61fc9b1c7f461aee2
Eric Mariacher <eric.mariacher@gmail.com>
2020-09-05 14:29:03 +0200

    next Euler37

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -8,28 +8,12 @@ import scala.math.BigInt
 class EulerMain extends FlatSpec with Matchers {
 
 
-  "Euler33" should "be OK" in {
-    println("Euler33")
+  "Euler37" should "be OK" in {
+    println("Euler37")
 
-    val p100 = (10 to 99).filter(_.toString.indexOf("0") < 0)
-
-    val z = p100.map(i => {
-      p100.dropWhile(_ <= i).filter(j => i.toString.toList.intersect(j.toString.toList).nonEmpty).map(j => {
-        val k = i.toString.toList.intersect(j.toString.toList).head
-        (i, j, k)
-      })
-    }).flatten.filter(ijk => {
-      ijk._1.toString.toList.exists(_ != ijk._3) && ijk._2.toString.toList.exists(_ != ijk._3)
-    }).map(ijk => {
-      val is = ijk._1.toString.toList.filter(_ != ijk._3).head.toString
-      val js = ijk._2.toString.toList.filter(_ != ijk._3).head.toString
-      (ijk._1, ijk._2, ijk._3, ijk._1.toDouble / ijk._2.toDouble, is, js, is.toDouble / js.toDouble)
-    }).filter(u => u._4 == u._7)
-    println(z.mkString("\n"))
-
-    val result = z.map(_._6.toInt).product / z.map(_._5.toInt).product
-    println("Euler33[" + result + "]")
-    result shouldEqual 100
+    val result = 0
+    println("Euler37[" + result + "]")
+    result shouldEqual 0
   }
 
 }
--- euler_sbt/src/test/scala/EulerSolved.scala
+++ euler_sbt/src/test/scala/EulerSolved.scala
@@ -246,6 +246,30 @@ class EulerSolved extends FlatSpec with Matchers {
     println("Euler32[" + result + "]")
     result shouldEqual 45228
   }
+  "Euler33" should "be OK" in {
+    println("Euler33")
+
+    val p100 = (10 to 99).filter(_.toString.indexOf("0") < 0)
+
+    val z = p100.map(i => {
+      p100.dropWhile(_ <= i).filter(j => i.toString.toList.intersect(j.toString.toList).nonEmpty).map(j => {
+        val k = i.toString.toList.intersect(j.toString.toList).head
+        (i, j, k)
+      })
+    }).flatten.filter(ijk => {
+      ijk._1.toString.toList.exists(_ != ijk._3) && ijk._2.toString.toList.exists(_ != ijk._3)
+    }).map(ijk => {
+      val is = ijk._1.toString.toList.filter(_ != ijk._3).head.toString
+      val js = ijk._2.toString.toList.filter(_ != ijk._3).head.toString
+      (ijk._1, ijk._2, ijk._3, ijk._1.toDouble / ijk._2.toDouble, is, js, is.toDouble / js.toDouble)
+    }).filter(u => u._4 == u._7)
+    println(z.mkString("\n"))
+
+    val result = z.map(_._6.toInt).product / z.map(_._5.toInt).product
+    println("Euler33[" + result + "]")
+    result shouldEqual 100
+  }
+
   "Euler38" should "be OK" in {
     println("Euler38")
 
