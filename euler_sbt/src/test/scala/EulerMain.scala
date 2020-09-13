03b6a7fe4641eec5c351b92cf94e40c3ce3e2a17
Eric Mariacher <eric.mariacher@gmail.com>
2020-09-13 08:33:52 +0200

    Euler37 merde! seulement 10 :-1:

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -16,32 +16,16 @@ class EulerMain extends FlatSpec with Matchers {
       bi == 0 || EulerPrime.isPrime(bi)
     }
 
-    def doZeJob(l: List[Int]): List[Int] = {
-      val z = l.flatMap(p => {
-        List((p * 10) + 3, (p * 10) + 7, (p * 10) + 9)
-      })
-
-      val y = z.intersect(premiers.toList)
-      val x = y.filter(p => {
-        EulerPrime.isPrime(p) & EulerPrime.isPrime(p % 100000) & EulerPrime.isPrime(p % 10000) & EulerPrime.isPrime(p % 1000) &
-          EulerPrime.isPrime(p % 100) & EulerPrime.isPrime(p % 10) &
-          isPrimeNZ(p / 100000) & isPrimeNZ(p / 10000) & isPrimeNZ(p / 1000) & isPrimeNZ(p / 100) & isPrimeNZ(p / 10)
-      })
-
-      println(z, y)
-      //val j = (l ++ x).distinct.sorted.filter(p => EulerPrime.isPrime(p))
-      println("*****" + x)
-      x
+    def OK(p: Int): Boolean = {
+      EulerPrime.isPrime(p) & EulerPrime.isPrime(p % 100000) & EulerPrime.isPrime(p % 10000) & EulerPrime.isPrime(p % 1000) &
+        EulerPrime.isPrime(p % 100) & EulerPrime.isPrime(p % 10) &
+        isPrimeNZ(p / 100000) & isPrimeNZ(p / 10000) & isPrimeNZ(p / 1000) & isPrimeNZ(p / 100) & isPrimeNZ(p / 10)
     }
 
     println(premiers)
-
-
     val root = List(23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797)
-    println(root.map(p => (p, isPrimeNZ(p))))
-    val l = doZeJob((10 to 1000).toList)
-    doZeJob(l)
-
+    println(root.map(p => (p, OK(p))))
+    println(premiers.filter(p => OK(p.toInt)))
 
     val result = 0
     println("Euler37[" + result + "]")
