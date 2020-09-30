fd285f2a9292501da6a5dfbde00459d7c09f9f26
Eric Mariacher <eric.mariacher@gmail.com>
2020-09-30 17:56:46 +0200

    Euler37 no good, giving up --> Euler25

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -8,29 +8,11 @@ import scala.math.BigInt
 class EulerMain extends FlatSpec with Matchers {
 
 
-  "Euler37" should "be OK" in {
-    println("Euler37")
-    val premiers = EulerPrime.premiers100000
-
-    def isPrimeNZ(bi: BigInt): Boolean = {
-      bi == 0 || EulerPrime.isPrime(bi)
-    }
-
-    def OK(p: Int): Boolean = {
-      EulerPrime.isPrime(p) & EulerPrime.isPrime(p % 100000) & EulerPrime.isPrime(p % 10000) & EulerPrime.isPrime(p % 1000) &
-        EulerPrime.isPrime(p % 100) & EulerPrime.isPrime(p % 10) &
-        isPrimeNZ(p / 100000) & isPrimeNZ(p / 10000) & isPrimeNZ(p / 1000) & isPrimeNZ(p / 100) & isPrimeNZ(p / 10)
-    }
-
-    val prems = premiers.filter(p => p.toString.indexOf("2") < 0 & p.toString.indexOf("4") < 0 & p.toString.indexOf("6") < 0 &
-      p.toString.indexOf("8") < 0 & p.toString.indexOf("0") < 0)
-    println(prems)
-    val root = List(23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797, 3173, 7973, 31373, 31797, 37973, 73137, 73797)
-    println(root.map(p => (p, OK(p))))
-    println(premiers.filter(p => OK(p.toInt)))
+  "Euler25" should "be OK" in {
+    println("Euler25")
 
     val result = 0
-    println("Euler37[" + result + "]")
+    println("Euler25[" + result + "]")
     result shouldEqual 0
   }
 
--- euler_sbt/src/test/scala/EulerNotYetSolved.scala
+++ euler_sbt/src/test/scala/EulerNotYetSolved.scala
@@ -10,6 +10,32 @@ import scala.math.BigInt
  * Created by emariacher on 21.03.2016.
  */
 class EulerNotYetSolved extends FlatSpec with Matchers {
+  "Euler37" should "be OK" in {
+    println("Euler37")
+    val premiers = EulerPrime.premiers100000
+
+    def isPrimeNZ(bi: BigInt): Boolean = {
+      bi == 0 || EulerPrime.isPrime(bi)
+    }
+
+    def OK(p: Int): Boolean = {
+      EulerPrime.isPrime(p) & EulerPrime.isPrime(p % 100000) & EulerPrime.isPrime(p % 10000) & EulerPrime.isPrime(p % 1000) &
+        EulerPrime.isPrime(p % 100) & EulerPrime.isPrime(p % 10) &
+        isPrimeNZ(p / 100000) & isPrimeNZ(p / 10000) & isPrimeNZ(p / 1000) & isPrimeNZ(p / 100) & isPrimeNZ(p / 10)
+    }
+
+    val prems = premiers.filter(p => p.toString.indexOf("2") < 0 & p.toString.indexOf("4") < 0 & p.toString.indexOf("6") < 0 &
+      p.toString.indexOf("8") < 0 & p.toString.indexOf("0") < 0)
+    println(prems)
+    val root = List(23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797, 3173, 7973, 31373, 31797, 37973, 73137, 73797)
+    println(root.map(p => (p, OK(p))))
+    println(premiers.filter(p => OK(p.toInt)))
+
+    val result = 0
+    println("Euler37[" + result + "]")
+    result shouldEqual 0
+  }
+
   "Euler211" should "be OK" in {
     println("Euler211")
 
