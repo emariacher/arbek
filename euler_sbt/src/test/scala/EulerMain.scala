412ded89c09d48e2b715e450114760371a45fe52
Eric Mariacher <eric.mariacher@gmail.com>
2020-10-03 07:59:47 +0200

    Euler25

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -11,6 +11,24 @@ class EulerMain extends FlatSpec with Matchers {
   "Euler25" should "be OK" in {
     println("Euler25")
 
+    def fib(h: BigInt, n: BigInt): LazyList[BigInt] = {
+      h #:: fib(n, h + n)
+    }
+
+    var x = fib(0, 1)
+    println(s"results: ${(x take 100).toList}")
+
+    def fib3(n: Int): Int = {
+      def fib_tail(n: Int, a: Int, b: Int): Int = n match {
+        case 0 => a
+        case _ => fib_tail(n - 1, b, a + b)
+      }
+
+      return fib_tail(n, 0, 1)
+    }
+
+    println(fib3(12))
+
     val result = 0
     println("Euler25[" + result + "]")
     result shouldEqual 0
