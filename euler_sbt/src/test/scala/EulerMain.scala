4298782f99367546662b4e70c85f5f0c92873ee0
Eric Mariacher <eric.mariacher@gmail.com>
2020-10-04 09:25:21 +0200

    Euler25

--- euler_sbt/src/test/scala/EulerMain.scala
+++ euler_sbt/src/test/scala/EulerMain.scala
@@ -19,8 +19,11 @@ class EulerMain extends FlatSpec with Matchers {
     println(s"results: ${(x take 100).toList}")
     var y = (x take 150).toList.zipWithIndex.map(a => (a, a._1.toString.length)).groupBy(_._2).toList.map(a => (a._1, a._2.length)).sortBy(_._1)
     println(y.mkString("\n"))
+    println("index of 1st fibonnacci with 31 digits: ", (x take 150).toList.zipWithIndex.map(a => (a, a._1.toString.length)).filter(_._2 == 31).head)
     var z = (x take 200).toList.zipWithIndex.map(a => (a, a._1.toString.length)).groupBy(_._2).toList.map(a => (a._1, a._2.length)).sortBy(_._1).map(_._2)
     println(z)
+    val pattern = List(4, 5, 5, 5, 4, 5, 5, 5, 5, 4, 5, 5, 5, 5)
+    println(pattern, pattern.sum)
 
     def fib3(n: Int): Int = {
       def fib_tail(n: Int, a: Int, b: Int): Int = n match {
