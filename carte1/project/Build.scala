916de33cf3961aca58368eb4a31d133a8e0c6863
Eric Mariacher <eric.mariacher@gmail.com>
2020-11-22 08:29:57 +0100

    Carte maintenance

--- carte1/project/Build.scala
+++ carte1/project/Build.scala
@@ -5,11 +5,12 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.scalamacros",
     version := "1.0.0",
-    scalaVersion := "2.11.6",
-    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.10.5", "2.11.0", "2.11.1", "2.11.2", "2.11.3", "2.11.4", "2.11.5", "2.11.6", "2.11.8"),
+    scalaVersion := "2.13.1",
+    crossScalaVersions := Seq("2.12.6", "2.13.1"),
     resolvers += Resolver.sonatypeRepo("snapshots"),
     resolvers += Resolver.sonatypeRepo("releases"),
-    scalacOptions ++= Seq()
+    logLevel := Level.Debug,
+    scalacOptions ++= Seq("-feature", "-deprecation")
   )
 }
 
@@ -18,31 +19,46 @@ object MyBuild extends Build {
   import BuildSettings._
 
   lazy val root: Project = Project(
-    "root",
+    "Carte_sbt",
     file("."),
     settings = buildSettings ++ Seq(
-      run <<= run in Compile in core)
+      logLevel := Level.Debug,
+      run := run in Compile in core)
   ) aggregate(macros, core)
 
   lazy val macros: Project = Project(
     "macros",
     file("macros"),
     settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
+      libraryDependencies <+= (scalaVersion) ("org.scala-lang" % "scala-reflect" % _),
+      logLevel := Level.Debug,
       libraryDependencies := {
         CrossVersion.partialVersion(scalaVersion.value) match {
           // if Scala 2.11+ is used, quasiquotes are available in the standard distribution
-          case Some((2, scalaMajor)) if scalaMajor >= 11 =>
+          case Some((2, scalaMajor)) if scalaMajor >= 13 =>
             libraryDependencies.value ++ Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.3.9",
-  "com.typesafe.akka" %% "akka-testkit" % "2.3.9",
-  "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-  "junit" % "junit" % "4.12" % "test",
-  "com.novocode" % "junit-interface" % "0.11" % "test"
-)  ++ Seq(
-              "org.scala-lang.modules" %% "scala-xml" % "1.0.3",
-              "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.3",
-              "org.scala-lang.modules" %% "scala-swing" % "1.0.1")
+              "com.typesafe.akka" %% "akka-actor" % "2.5.25",
+              "com.typesafe.akka" %% "akka-testkit" % "2.5.25",
+              "org.scalatest" %% "scalatest" % "3.2.0-M1" % "test",
+              "junit" % "junit" % "4.12" % "test",
+              "com.novocode" % "junit-interface" % "0.11" % "test",
+              "org.scala-lang.modules" %% "scala-xml" % "1.2.0",
+              "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
+              "org.apache.commons" % "commons-math3" % "3.5",
+              "com.lihaoyi" %% "sourcecode" % "0.1.7",
+              "org.scala-lang.modules" %% "scala-swing" % "2.1.1")
+          case Some((2, scalaMajor)) if scalaMajor >= 12 =>
+            libraryDependencies.value ++ Seq(
+              "com.typesafe.akka" %% "akka-actor" % "2.5.19",
+              "com.typesafe.akka" %% "akka-testkit" % "2.5.19",
+              "org.scalatest" %% "scalatest" % "3.0.5" % "test",
+              "junit" % "junit" % "4.12" % "test",
+              "com.novocode" % "junit-interface" % "0.11" % "test",
+              "com.lihaoyi" %% "sourcecode" % "0.1.4",
+              "org.scala-lang.modules" %% "scala-xml" % "1.1.1",
+              "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.1",
+              "org.apache.commons" % "commons-math3" % "3.5",
+              "org.scala-lang.modules" %% "scala-swing" % "2.1.0")
           // in Scala 2.10, quasiquotes are provided by macro paradise
           case Some((2, 10)) =>
             libraryDependencies.value ++ Seq(
@@ -66,7 +82,7 @@ object MyBuild extends Build {
             // or just libraryDependencies.value if you don't depend on scala-swing
             libraryDependencies.value :+ "org.scala-lang" % "scala-swing" % scalaVersion.value
         }
-      }
-  )
-  ) dependsOn(macros)
+      },logLevel := Level.Debug
+    )
+  ) dependsOn (macros)
 }
