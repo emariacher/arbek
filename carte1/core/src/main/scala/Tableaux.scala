45b5c040403761b3f21a9ba83af231c491f178fc
Eric Mariacher <eric.mariacher@gmail.com>
2020-11-26 16:51:44 +0100

    Carte Maintenance

--- carte1/core/src/main/scala/Tableaux.scala
+++ carte1/core/src/main/scala/Tableaux.scala
@@ -9,23 +9,23 @@ import kebra.MyLog._
 object Tableaux {
   var tbx: Tableaux = _
 
-  def newTbx(zp: ZePanel, maxRow: Int, maxCol: Int, size: Dimension, origin: Dimension) {
+  def newTbx(zp: ZePanel, maxRow: Int, maxCol: Int, size: Dimension, origin: Dimension): Unit = {
     tbx = new Tableaux(zp, maxRow, maxCol, size, origin)
-    myPrintIt(FrontiereV.values,CantonTypeV.values)
+    myPrintIt(FrontiereV.values, CantonTypeV.values)
   }
 }
 
 class Tableaux(val zp: ZePanel, val maxRow: Int, val maxCol: Int, val size: Dimension, val origin: Dimension) {
-  val seeds = List(0, -2002675365, -1995825069, -2000188942, -2003116831, -1996108355, -1995594583)
-  var state = StateMachine.generate
+  val seeds: List[Int] = List(0, -2002675365, -1995825069, -2000188942, -2003116831, -1996108355, -1995594583)
+  var state: StateMachine = StateMachine.generate
   var cb = new CarteBlanche
   var carte: Coloriage = _
   var seedIndex = 0
-  var seed = getNextSeed
+  var seed: Int = getNextSeed
   var rnd = new Random(seed)
   var count = 0
 
-  def doZeJob(command: String, timeout: Int) {
+  def doZeJob(command: String, timeout: Int): Unit = {
     //				System.out.println(MyLog.tag(1)+" state: "+StateMachine.nameOf(state)+" "+count)
     zp.lbl.text = "Seed: " + seed + ",  " + cb + ", CountSteps: " + count
     state match {
