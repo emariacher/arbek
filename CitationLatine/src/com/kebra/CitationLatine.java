5b091219b16e8804ea328b7ca0ca3270dbc8ba4b
Eric Mariacher <eric.mariacher@gmail.com>
2021-02-23 19:18:03 +0100

    Citation latine

--- CitationLatine/src/com/kebra/CitationLatine.java
+++ CitationLatine/src/com/kebra/CitationLatine.java
@@ -49,7 +49,7 @@ public class CitationLatine {
         String base64encodedString = new String();
         try {
 
-            System.out.println("plain:    " + text);
+            System.out.println("plain:    <<<<<<<<<<<<< " + text +" >>>>>>>>>>>>>>");
 
             String seizetext = padRightMod16(text);
             System.out.println("seizetext[" + seizetext + "] " + seizetext.length());
