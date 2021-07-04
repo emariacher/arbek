0d9201b9a3af5b6fa2ec28c9072cd73c2c9d5d6c
Eric Mariacher <eric.mariacher@gmail.com>
2021-07-04 08:16:02 +0200

    dotty1

--- dotty1/src/test/scala/Test1.scala
+++ dotty1/src/test/scala/Test1.scala
@@ -2,5 +2,9 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class Test1:
-  @Test def t1(): Unit = 
+  @Test def t1(): Unit =
     assertEquals("I was compiled by Scala 3. :)", msg)
+  @Test def t2(): Unit = {
+    hello
+    assertEquals("I was compiled by Scala 2. :)", msg)
+  }
